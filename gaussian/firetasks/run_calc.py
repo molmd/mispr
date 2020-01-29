0ad6350f10afe3327f44b762744023bc5e67fd2b
Rasha <atwi.r@husky.neu.edu>
2020-01-29 10:51:40 -0500

    Added the start of Gaussian run firetask

--- gaussian/firetasks/run_calc.py
+++ gaussian/firetasks/run_calc.py
@@ -7,15 +7,26 @@ from fireworks.utilities.fw_utilities import explicit_serialize
 
 @explicit_serialize
 class RunGaussianDirectTest(FiretaskBase):
+    optional_params = ["working_dir", "input_file"]
+
     def run_task(self, fw_spec):
-        cmd = f"g09 < {fw_spec['input_file_path']} > " \
-              f"{os.path.splitext(fw_spec['input_file_path'])[0]}.out"
+        working_dir = fw_spec.get('working_dir', self.get("working_dir", os.getcwd()))
+
+        input_file = fw_spec.\
+            get('input_file', self.get('input_file', 'mol.com'))
+        input_path = os.path.join(working_dir, input_file)
+
+        output_file = f"{os.path.splitext(input_file)[0]}.out"
+        output_path = os.path.join(working_dir, output_file)
+
+        cmd = f"g09 < {input_path} > {output_path}"
         print("Running command: {}".format(cmd))
         return_code = subprocess.call(cmd, shell=True)
-        print("Command {} finished running with return code: {}".format(
-            cmd, return_code))
-        fw_spec['output_path'] = f"{os.path.splitext(fw_spec['input_file_path'])[0]}.out"
-        return FWAction(update_spec={'output_path': fw_spec['output_path']})
+        print("Finished running with return code: {}".format(return_code))
+
+        update_spec = {'working_dir': working_dir, 'input_file': input_file,
+                       'output_file': output_file}
+        return FWAction(update_spec=update_spec)
 
 
 @explicit_serialize
@@ -28,4 +39,3 @@ class RunGaussianCustodian(FiretaskBase):
 class RunGaussianFake(FiretaskBase):
     def run_task(self, fw_spec):
         pass
-
