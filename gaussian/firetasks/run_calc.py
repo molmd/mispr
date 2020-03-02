576c38646c076c4d6ec06bc3ff41e105a9f5bd7e
Rasha <atwi.r@husky.neu.edu>
2020-03-02 14:07:15 -0500

    Take Gaussian run command as an input

--- gaussian/firetasks/run_calc.py
+++ gaussian/firetasks/run_calc.py
@@ -1,17 +1,22 @@
 import os
+import logging
 import subprocess
+from configparser import ConfigParser
 
-from fireworks.core.firework import FiretaskBase, FWAction, Firework
+from fireworks.core.firework import FiretaskBase
+from fireworks.fw_config import CONFIG_FILE_DIR
 from fireworks.utilities.fw_utilities import explicit_serialize
+
+logger = logging.getLogger(__name__)
 
 
 @explicit_serialize
 class RunGaussianDirect(FiretaskBase):
-    # required_params = ["gaussian_cmd"]
-    optional_params = ["working_dir", "input_file", "output_file"]
+    required_params = []
+    optional_params = ["working_dir", "input_file", "output_file",
+                       "gaussian_cmd"]
 
     def run_task(self, fw_spec):
-        # cmd = self["gaussian_cmd"]
         working_dir = self.get("working_dir", os.getcwd())
 
         input_file = self.get('input_file', 'mol.com')
@@ -20,10 +25,17 @@ class RunGaussianDirect(FiretaskBase):
         output_file = self.get('output_file', 'mol.out')
         output_path = os.path.join(working_dir, output_file)
 
-        cmd = f"g09 < {input_path} > {output_path}"
-        print("Running command: {}".format(cmd))
+        cmd = self.get("gaussian_cmd")
+        if not cmd:
+            cfg = ConfigParser()
+            cfg.read(CONFIG_FILE_DIR+'/config.ini')
+            cmd = cfg['RunCalc']['gcmd']
+        cmd = cmd.replace('$input_path$', input_path).\
+            replace('$output_path$', output_path)
+
+        logger.info("Running command: {}".format(cmd))
         return_code = subprocess.call(cmd, shell=True)
-        print("Finished running with return code: {}".format(return_code))
+        logger.info("Finished running with return code: {}".format(return_code))
 
 
 @explicit_serialize
