ebb0bbbb9d464f0d3770e737b2dbb2f1ac828a82
Rasha <atwi.r@husky.neu.edu>
2020-05-09 22:22:22 -0400

    Correct order of fireworks in BE workflow

--- infrastructure/gaussian/workflows/base_standard.py
+++ infrastructure/gaussian/workflows/base_standard.py
@@ -2,7 +2,6 @@ import os
 import logging
 
 from fireworks import Firework, Workflow
-from pymatgen.core.structure import Molecule
 from infrastructure.gaussian.firetasks.parse_outputs import \
     BindingEnergytoDB
 from infrastructure.gaussian.firetasks.parse_outputs import ProcessRun
@@ -261,21 +260,20 @@ def get_binding_energies(mol_operation_type,
                          oxidation_states=None,
                          skip_opt_freq=None,
                          **kwargs):
+
     # mol_operation_type = [], mol = [], index = []
     # order of the indices should be consistent with the order of the mols
     fws = []
     molecules = []
     working_dir = working_dir or os.getcwd()
-    key1 = 'linking_mol'
-    key2 = 'linked_mol'
-    key3 = 'final_mol'
+    keys = ["mol_1", "mol_2", "mol_linked"]
 
     if skip_opt_freq is None:
         skip_opt_freq = [False, False]
-
+    parents = []
     for position, [operation, molecule, key, skip] in \
             enumerate(
-                zip(mol_operation_type, mol, [key1, key2], skip_opt_freq)):
+                zip(mol_operation_type, mol, keys[:2], skip_opt_freq)):
         mol_object, opt_freq_init_fws = \
             common_fw(mol_operation_type=operation,
                       mol=molecule,
@@ -289,6 +287,7 @@ def get_binding_energies(mol_operation_type,
                       skip_opt_freq=skip,
                       **kwargs)
         fws += opt_freq_init_fws
+        parents.append(len(fws))
         molecules.append(mol_object)
 
     final_mol_formula = "{}_{}".format(get_mol_formula(molecules[0]),
@@ -298,40 +297,36 @@ def get_binding_energies(mol_operation_type,
                                       mol={"operation_type": [
                                           "get_from_run_dict",
                                           "get_from_run_dict"],
-                                           "mol": [key1, key2],
-                                           "index": index,
-                                           "bond_order": bond_order},
+                                          "mol": keys[:2],
+                                          "index": index,
+                                          "bond_order": bond_order},
                                       working_dir=working_dir,
                                       db=db,
+                                      filename=final_mol_formula,
                                       opt_gaussian_inputs=opt_gaussian_inputs,
                                       freq_gaussian_inputs=freq_gaussian_inputs,
                                       cart_coords=cart_coords,
                                       oxidation_states=oxidation_states,
                                       process_mol_func=False,
                                       mol_name=final_mol_formula,
-                                      gout_key=key3,
+                                      gout_key=keys[-1],
                                       from_fw_spec=True,
                                       **kwargs)
     fws += opt_freq_final_fws
-
-    fw_analysis = Firework(BindingEnergytoDB(keys=[key1, key2, key3],
-                                             prop="final_energy",
-                                             main_run_key=key3,
-                                             new_prop="binding_energy_{}_{}_eV".
-                                             format(
-                                                 molecules[0].species[index[0]],
-                                                 molecules[1].species[index[1]]),
-                                             db=db),
-                           parents=fws[:],
-                           name="{}-{}".format(final_mol_formula,
-                                               "binding_energy_analysis"))
+    links_dict = {fws[i-1]: fws[-len(opt_freq_final_fws)] for i in parents}
+    fw_analysis = Firework(
+        BindingEnergytoDB(index=index,
+                          db=db,
+                          **{i: j for i, j in kwargs.items()
+                             if i in BindingEnergytoDB.required_params +
+                             BindingEnergytoDB.optional_params}),
+        parents=fws[:],
+        name="{}-{}".format(final_mol_formula,
+                            "binding_energy_analysis"))
     fws.append(fw_analysis)
 
-    name = "{}_{}".format(final_mol_formula, name)
-    links_dict = {fws[1]: fws[4], fws[3]: fws[4]}
-
     return Workflow(fws,
-                    name=name,
+                    name="{}_{}".format(final_mol_formula, name),
                     links_dict=links_dict,
                     **{i: j for i, j in kwargs.items()
                        if i in WORKFLOW_KWARGS})
