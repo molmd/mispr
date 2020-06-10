e55194889e1849c7eaea9b019226979847a27ade
Rasha <atwi.r@husky.neu.edu>
2020-06-10 10:23:57 -0400

    Identify the parents of a given firework in the IP/EA workflow

--- infrastructure/gaussian/workflows/base_standard.py
+++ infrastructure/gaussian/workflows/base_standard.py
@@ -368,6 +368,9 @@ def get_ip_ea(mol_operation_type,
               num_electrons=1,
               solvent_gaussian_inputs=None,
               solvent_properties=None,
+              states=None,
+              phases=None,
+              electrode_potentials=None,
               db=None,
               name="ip_ea_calculation",
               working_dir=None,
@@ -376,175 +379,127 @@ def get_ip_ea(mol_operation_type,
               cart_coords=True,
               skip_opt_freq=False,
               **kwargs):
-    # TODO: check the working dirs ; they are wrong
-    # TODO: check the links between the fireworks
-    # TODO: check with the group if they want to skip opt and freq
-    # TODO: check with the group if the order of optimizations is correct
-    # TODO: cleanup the messy code
-    # TODO: check with the group if one electron is exchanged only
-    # TODO: check if the group if we want to skip either IP or EA
+    def _create_fireworks(local_state, local_phase):
+        local_key = "{}_{}".format(local_state.lower(), local_phase.lower())
+        parent_state, parent_phase = _get_parent(local_state, local_phase)
+        parent_key = None
+        if parent_state in states and parent_phase in phases:
+            parent_key = "{}_{}".format(parent_state.lower(),
+                                        parent_phase.lower())
+        if not parent_key:
+            local_mol_op_type = mol_operation_type
+            local_mol = mol
+            local_skip = skip_opt_freq
+            local_process_mol_func = True
+            local_mol_name = None
+            local_from_fw_spec = False
+            local_dir_head = None
+        else:
+            local_mol_op_type = 'get_from_run_dict'
+            local_mol = parent_key
+            local_skip = False
+            local_process_mol_func = False
+            local_mol_name = "{}_{}".format(mol_formula, local_phase.lower())
+            local_from_fw_spec = True
+            local_dir_head = mol_formula
+
+        add_charge = 1 if local_state.lower() == 'cation' \
+            else -1 if local_state.lower() == 'anion' else 0
+        add_charge *= num_electrons
+
+        local_opt_gins = deepcopy(opt_gaussian_inputs)
+        local_freq_gins = deepcopy(freq_gaussian_inputs)
+        local_opt_gins['charge'] = ref_charge + add_charge
+        local_freq_gins['charge'] = ref_charge + add_charge
+        local_opt_gins["spin_multiplicity"] = spin_multiplicities[local_state]
+        local_freq_gins["spin_multiplicity"] = spin_multiplicities[local_state]
+
+        if local_phase.lower() == 'solution':
+            if "generic" in solvent_gaussian_inputs.lower() \
+                    and not solvent_properties:
+                raise Exception(
+                    "A generic solvent is provided as an input without "
+                    "specifying its parameters.")
+            local_opt_gins["route_parameters"]["SCRF"] = \
+                solvent_gaussian_inputs
+            local_freq_gins["route_parameters"]["SCRF"] = \
+                solvent_gaussian_inputs
+            if solvent_properties:
+                if "input_parameters" not in local_opt_gins:
+                    local_opt_gins["input_parameters"] = {}
+                local_opt_gins["input_parameters"].update(solvent_properties)
+
+        local_mol, local_fws = \
+            common_fw(mol_operation_type=local_mol_op_type,
+                      mol=local_mol,
+                      working_dir=working_dir,
+                      db=db,
+                      opt_gaussian_inputs=local_opt_gins,
+                      freq_gaussian_inputs=local_freq_gins,
+                      cart_coords=cart_coords,
+                      oxidation_states=None,
+                      process_mol_func=local_process_mol_func,
+                      mol_name=local_mol_name,
+                      dir_head=local_dir_head,
+                      gout_key=local_key,
+                      skip_opt_freq=local_skip,
+                      dir_structure=[local_state, local_phase],
+                      from_fw_spec=local_from_fw_spec,
+                      **kwargs)
+        fireworks_dict[local_key] = local_fws
+        if parent_key:
+            parents_dict[local_key] = parent_key
+        return local_fws  # local_mol, local_fws, fireworks_dict
+
+    def _get_parent(local_state, local_phase):
+        if local_phase.lower() == "solution":
+            if 'gas' in [k.lower() for k in states]:
+                return local_state, "gas"
+        if local_state.lower() == "cation" or local_state.lower() == "anion":
+            return "reference", local_state
+        else:
+            return None, None
 
     fws = []
-    parents = []
+    fireworks_dict = {}
+    links_dict = {}
+    parents_dict = {}
     working_dir = working_dir or os.getcwd()
     mol = recursive_relative_to_absolute_path(mol, working_dir)
-
-    keys = ["neutral_gas", "anion_gas", "cation_gas",
-            "neutral_sol", "anion_sol", "cation_sol"]
 
     if ref_charge != opt_gaussian_inputs.get("charge", ref_charge):
         raise Exception("The provided reference charge is not consistent with "
                         "the one found in the gaussian input parameters.")
-    sol_opt_gaussian_inputs = opt_gaussian_inputs
-    sol_freq_gaussian_inputs = freq_gaussian_inputs
-    gaussian_inputs = [opt_gaussian_inputs, freq_gaussian_inputs,
-                       sol_opt_gaussian_inputs, sol_freq_gaussian_inputs]
-    for i in gaussian_inputs:
-        i["charge"] = ref_charge
-        i["spin_multiplicity"] = spin_multiplicities["neutral"]
 
-    if "generic" in solvent_gaussian_inputs.lower() and not solvent_properties:
-        raise Exception("A generic solvent is provided as an input without "
-                        "specifying its parameters.")
-    sol_opt_gaussian_inputs["route_parameters"]["SCRF"] = \
-        solvent_gaussian_inputs
-    sol_freq_gaussian_inputs["route_parameters"]["SCRF"] = \
-        solvent_gaussian_inputs
-    if solvent_properties:
-        if "input_parameters" not in sol_opt_gaussian_inputs:
-            sol_opt_gaussian_inputs["input_parameters"] = {}
-        sol_opt_gaussian_inputs["input_parameters"].update(solvent_properties)
+    # TODO: Cleanup this part
+    if states is None:
+        states = ["reference", "cation", "anion"]
+    if phases is None:
+        phases = ["gas", "solution"]
 
-    mol, neutral_gas_fws = common_fw(mol_operation_type=mol_operation_type,
-                                     mol=mol,
-                                     working_dir=working_dir,
-                                     db=db,
-                                     opt_gaussian_inputs=opt_gaussian_inputs,
-                                     freq_gaussian_inputs=freq_gaussian_inputs,
-                                     cart_coords=cart_coords,
-                                     oxidation_states=None,
-                                     gout_key="neutral_gas",
-                                     skip_opt_freq=skip_opt_freq,
-                                     dir_structure=["Neutral", "Gas"],
-                                     **kwargs)
-    fws += neutral_gas_fws
+    for state in states:
+        if state.lower() not in ["reference", "cation", "anion"]:
+            raise ValueError("The provided states are not supported. Supported "
+                             "ones are reference, cation, and/or anion.")
+    for phase in phases:
+        if phase.lower() not in ["gas", "solution"]:
+            raise ValueError("The provided phases are not supported. Supported "
+                             "ones are gas and/or solution.")
+
+    for state, phase in itertools.product(states, phases):
+        fws += _create_fireworks(state, phase)
     mol_formula = get_mol_formula(mol)
-    # parents.append(len(fws))
-
-    _, neutral_sol_fws = common_fw(mol_operation_type="get_from_run_dict",
-                                   mol="neutral_gas",
-                                   working_dir=working_dir,
-                                   db=db,
-                                   opt_gaussian_inputs=sol_opt_gaussian_inputs,
-                                   freq_gaussian_inputs=sol_freq_gaussian_inputs,
-                                   cart_coords=cart_coords,
-                                   oxidation_states=None,
-                                   process_mol_func=False,
-                                   mol_name="{}_sol".format(mol_formula),
-                                   dir_head=mol_formula,
-                                   gout_key="neutral_sol",
-                                   skip_opt_freq=False,
-                                   dir_structure=["Neutral", "Solution"],
-                                   from_fw_spec=True,
-                                   **kwargs)
-    fws += neutral_sol_fws
-    # parents.append(len(fws))
-
-    for i in gaussian_inputs[:2]:
-        i["charge"] = ref_charge - num_electrons
-        i["spin_multiplicity"] = spin_multiplicities["anion"]
-
-    _, anion_gas_fws = common_fw(mol_operation_type="get_from_run_dict",
-                                 mol="neutral_gas",
-                                 working_dir=working_dir,
-                                 db=db,
-                                 opt_gaussian_inputs=opt_gaussian_inputs,
-                                 freq_gaussian_inputs=freq_gaussian_inputs,
-                                 cart_coords=cart_coords,
-                                 oxidation_states=None,
-                                 process_mol_func=False,
-                                 mol_name="{}-_gas".format(mol_formula),
-                                 dir_head=mol_formula,
-                                 gout_key="anion_gas",
-                                 skip_opt_freq=False,
-                                 dir_structure=["Anion", "Gas"],
-                                 from_fw_spec=True,
-                                 **kwargs)
-    fws += anion_gas_fws
-    # parents.append(len(fws))
-
-    for i in gaussian_inputs[:2]:
-        i["charge"] = ref_charge + num_electrons
-        i["spin_multiplicity"] = spin_multiplicities["cation"]
-
-    _, cation_gas_fws = common_fw(mol_operation_type="get_from_run_dict",
-                                  mol="neutral_gas",
-                                  working_dir=working_dir,
-                                  db=db,
-                                  opt_gaussian_inputs=opt_gaussian_inputs,
-                                  freq_gaussian_inputs=freq_gaussian_inputs,
-                                  cart_coords=cart_coords,
-                                  oxidation_states=None,
-                                  process_mol_func=False,
-                                  mol_name="{}+_gas".format(mol_formula),
-                                  dir_head=mol_formula,
-                                  gout_key="cation_gas",
-                                  skip_opt_freq=False,
-                                  dir_structure=["Cation", "Gas"],
-                                  from_fw_spec=True,
-                                  **kwargs)
-    fws += cation_gas_fws
-    # parents.append(len(fws))
-
-    for i in gaussian_inputs[2:]:
-        i["charge"] = ref_charge - num_electrons
-        i["spin_multiplicity"] = spin_multiplicities["anion"]
-
-    _, anion_sol_fws = common_fw(mol_operation_type="get_from_run_dict",
-                                 mol="anion_gas",
-                                 working_dir=working_dir,
-                                 db=db,
-                                 opt_gaussian_inputs=opt_gaussian_inputs,
-                                 freq_gaussian_inputs=freq_gaussian_inputs,
-                                 cart_coords=cart_coords,
-                                 oxidation_states=None,
-                                 process_mol_func=False,
-                                 mol_name="{}-_sol".format(mol_formula),
-                                 dir_head=mol_formula,
-                                 gout_key="anion_sol",
-                                 skip_opt_freq=False,
-                                 dir_structure=["Anion", "Solution"],
-                                 from_fw_spec=True,
-                                 **kwargs)
-    fws += anion_sol_fws
-    # parents.append(len(fws))
-
-    for i in gaussian_inputs[2:]:
-        i["charge"] = ref_charge + num_electrons
-        i["spin_multiplicity"] = spin_multiplicities["cation"]
-
-    _, cation_sol_fws = common_fw(mol_operation_type="get_from_run_dict",
-                                  mol="cation_gas",
-                                  working_dir=working_dir,
-                                  db=db,
-                                  opt_gaussian_inputs=opt_gaussian_inputs,
-                                  freq_gaussian_inputs=freq_gaussian_inputs,
-                                  cart_coords=cart_coords,
-                                  oxidation_states=None,
-                                  process_mol_func=False,
-                                  mol_name="{}+_sol".format(mol_formula),
-                                  dir_head=mol_formula,
-                                  gout_key="cation_sol",
-                                  skip_opt_freq=False,
-                                  dir_structure=["Cation", "Solution"],
-                                  from_fw_spec=True,
-                                  **kwargs)
-    fws += cation_sol_fws
-    # parents.append(len(fws))
-    links_dict = {fws[1]: [fws[2], fws[4], fws[6]], fws[5]: fws[8],
-                  fws[7]: fws[10]}
+    for i, j in parents_dict.items():
+        links_dict[fireworks_dict[j][1]] = \
+            links_dict.get(fireworks_dict[j][1], []) + [fireworks_dict[j][0]]
     fw_analysis = Firework(
         IPEAtoDB(num_electrons=num_electrons,
+                 states=states,
+                 phases=phases,
+                 solvent_gaussian_inputs=solvent_gaussian_inputs,
+                 solvent_properties=solvent_properties,
+                 electrode_potentials=electrode_potentials,
+                 parents_dict=parents_dict,
                  db=db,
                  **{i: j for i, j in kwargs.items()
                     if i in BindingEnergytoDB.required_params +
