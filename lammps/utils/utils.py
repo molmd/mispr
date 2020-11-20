08a2adbeee4381b54f0c1b05e34a195c40c69f22
mmbliss <matthew.bliss@stonybrook.edu>
2020-11-20 14:23:19 -0500

    LAMMPS Infrastructure Update
    Adds LAMMPS Infrastructure.
    Current finished workflow allows creation
    of data files from Gaussian Output,
    AMBER prmtop, or dictionaries of ff
    parameters. Box can be rectangular.

---
+++ lammps/utils/utils.py
@@ -0,0 +1,104 @@
+import numpy as np
+from collections import OrderedDict
+
+def add_ff_labels_to_BADI_lists(list, label):
+    """
+    Adds extra string to the end of all atom type labels in lists containing
+    information about Bonds, Angles, Dihedrals, or Impropers (BADI). This
+    function is intended to be used through the add_ff_labels_to_dict()
+    function.
+    :param list: [List] The value from ff_dict using one of the following keys:
+                 'Bonds', 'Angles', 'Dihedrals', or 'Impropers'.
+                 The form of this list should be as follows:
+                 [{'coeffs': [Float, ...], 'types': [(Str, ...), ...]}, ...]
+    :param label: [Str] A label for the molecular species that is unique for
+                  the system being created.
+    :return:
+    """
+    output_badi_list = []
+    for dict in list:
+        new_types = []
+        for type in dict['types']:
+            new_types.append(tuple(atom + label for atom in type))
+        output_badi_list.append({'coeffs': dict['coeffs'], 'types': new_types})
+    return output_badi_list
+
+
+def add_ff_labels_to_dict(ff_dict, label):
+    """
+
+    :param ff_dict:
+                    {'Molecule':   pmg.Molecule,
+                     'Labels':     List,
+                     'Masses':     OrderedDict,
+                     'Nonbond':    List,
+                     'Bonds':      [{'coeffs': [a, b], 'types': [('x1', 'x2'), ...]}, ...],
+                     'Angles':     [{'coeffs': [a, b], 'types': [('x1', 'x2', 'x3'), ...]}, ...],
+                     'Dihedrals':  [{'coeffs': [a, b, c], 'types': [('x1', 'x2', 'x3', 'x4), ...]}, ...],
+                     'Impropers':  [{'coeffs': [a, b, c], 'types': [('x1', 'x2', 'x3', 'x4), ...]}, ...],
+                     'Improper Topologies': List,
+                     'Charges':    np.Array,
+                     ...}
+    :param label:
+    :return:
+    """
+
+    output_labels = [old_label + label for old_label in ff_dict['Labels']]
+
+    output_masses = OrderedDict()
+    for atom_type, mass in ff_dict['Masses'].items():
+        output_masses[atom_type + label] = mass
+
+    output_bonds = add_ff_labels_to_BADI_lists(ff_dict['Bonds'], label)
+    output_angles = add_ff_labels_to_BADI_lists(ff_dict['Angles'], label)
+    output_dihedrals = add_ff_labels_to_BADI_lists(ff_dict['Dihedrals'], label)
+    output_impropers = add_ff_labels_to_BADI_lists(ff_dict['Impropers'], label)
+
+    output_ff_dict = {'Molecule':   ff_dict['Molecule'],
+                      'Labels':     output_labels,
+                      'Masses':     output_masses,
+                      'Nonbond':    ff_dict['Nonbond'],
+                      'Bonds':      output_bonds,
+                      'Angles':     output_angles,
+                      'Dihedrals':  output_dihedrals,
+                      'Impropers':  output_impropers,
+                      'Improper Topologies': ff_dict['Improper Topologies'],
+                      'Charges':    ff_dict['Charges']}
+
+    return output_ff_dict
+
+
+if __name__ == "__main__":
+    Spce_label = ''
+    test_mass = OrderedDict({'ow' + Spce_label: 16.000, 'hw' + Spce_label: 1.008})
+    test_mass_2 = OrderedDict()
+
+    for key in test_mass.keys():
+        test_mass_2[key + 'H2O'] = test_mass[key]
+
+    print(test_mass)
+    print(test_mass_2, '\n')
+
+    test_bonds = [{'coeffs': [553.0, 1], 'types': [('ow' + Spce_label, 'hw' + Spce_label)]},
+                  {'coeffs': [552.0, 1], 'types': [('ow1' + Spce_label, 'hw1' + Spce_label)]}]
+
+    test_bonds_2 = add_ff_labels_to_BADI_lists(test_bonds, 'H2O')
+
+    print(test_bonds)
+    print(test_bonds_2, '\n')
+
+    Spce_param_dict = {'Molecule': 'Spce_molecule',
+                       'Labels': ['ow' + Spce_label, 'hw' + Spce_label, 'hw' + Spce_label],
+                       'Masses': OrderedDict({'ow' + Spce_label: 16.000, 'hw' + Spce_label: 1.008}),
+                       'Nonbond': [[0.155394259, 3.16555789], [0.0, 0.0]],
+                       'Bonds': [{'coeffs': [553.0, 1], 'types': [('ow' + Spce_label, 'hw' + Spce_label)]}],
+                       'Angles': [{'coeffs': [100.0, 109.47],
+                                   'types': [('hw' + Spce_label, 'ow' + Spce_label, 'hw' + Spce_label)]}],
+                       'Dihedrals': [],
+                       'Impropers': [],
+                       'Improper Topologies': None,
+                       'Charges': np.asarray([-0.8476, 0.4238, 0.4238])}
+    Spce_param_dict_2 = add_ff_labels_to_dict(Spce_param_dict, 'H2O')
+
+    print(Spce_param_dict)
+    print(Spce_param_dict_2)
---
+++ lammps/workflows/base_custom.py
@@ -0,0 +1,116 @@
+import os
+from fireworks.core.firework import Firework, Workflow
+from infrastructure.gaussian.utils.utils import get_mol_formula
+from infrastructure.lammps.firetasks.write_inputs import WriteDataFile
+from infrastructure.lammps.fireworks.custom_fw import GetFFDictFW
+
+def write_lammps_data(system_species_data,
+                      system_mixture_type,
+                      box_data,
+                      box_data_type = 'cubic',
+                      data_file_name = "complex.data",
+                      working_dir=None,
+                      **kwargs):
+    """
+
+    :param system_species_data: [dict]
+              {
+              species1_label: {"molecule": pmg.Molecule,
+                               "ff_param_method": str ("get_from_esp",
+                                                       "get_from_prmtop",
+                                                       "get_from_dict";
+                                                       defaults to "get_from_esp"),
+                               "ff_param_data": str (path to file) or dict (unlabeled ff_dict),
+                               "mol_mixture_type": "Solutes" or "Solvents",
+                               "mixture_data": int or dict},
+              ...,
+              speciesN_label: {...}
+              }
+    :param system_mixture_type: [str] "concentration" or "number of molecules"
+    :param box_data: [float, int, list (3,2), array (3,2), or LammpsBox] Definitions for box size.
+        See box_data_type for info how to define this parameter.
+    :param box_data_type: [string] Can be one of the following: 'cubic', 'rectangular', or 'LammpsBox'.
+        If 'cubic', box_data must be a float or int; if 'rectangular', box_data must be an array-like
+        with size (3,2); if 'LammpsBox', box_data must be a pymatgen.io.lammps.data.LammpsBox object.
+        Defaults to 'cubic'
+    :return:
+    """
+
+    fireworks = []
+    if system_mixture_type == "concentration":
+        system_mixture_data = {"Solutes": {},
+                               "Solvents": {}}
+    elif system_mixture_type == "number of molecules":
+        system_mixture_data = {}
+
+    links_dict = {}
+
+    for species, ff_data in system_species_data.items():
+        mol2_filename = "{}.mol2".format(species)
+        frcmod_filename = "{}.frcmod".format(species)
+        prmtop_filename = "{}.prmtop".format(species)
+        inpcrd_filename = "{}.inpcrd".format(species)
+
+        cur_firework = GetFFDictFW(ff_data["molecule"],
+                                   ff_data["ff_param_data"],
+                                   operation_type = ff_data["ff_param_method"],
+                                   label = species,
+                                   working_dir = working_dir,
+                                   output_filename_a = mol2_filename,
+                                   input_filename_p = mol2_filename,
+                                   output_filename_p = frcmod_filename,
+                                   prmtop_filename = prmtop_filename,
+                                   tleap_settings = {"mol2_file_path": os.path.join(working_dir, mol2_filename),
+                                                     "frcmod_file_path": os.path.join(working_dir, frcmod_filename),
+                                                     "prmtop_file_path": os.path.join(working_dir, prmtop_filename),
+                                                     "inpcrd_file_path": os.path.join(working_dir, inpcrd_filename)},
+                                   **kwargs)
+
+        fireworks.append(cur_firework)
+
+        links_dict[cur_firework.fw_id] = []
+        # print(cur_firework.fw_id)
+        if system_mixture_type == "concentration":
+            system_mixture_data[ff_data["mol_mixture_type"]][species] = ff_data["mixture_data"]
+        elif system_mixture_type == "number of molecules":
+            system_mixture_data[species] = ff_data["mixture_data"]
+
+    spec = kwargs.pop("spec", {})
+    firework2 = Firework([WriteDataFile(working_dir = working_dir,
+                                        data_filename = data_file_name,
+                                        system_mixture_data_type = system_mixture_type,
+                                        system_mixture_data = system_mixture_data,
+                                        system_box_data = box_data,
+                                        system_box_data_type = box_data_type,
+                                        **{i: j for i, j in kwargs.items() if i in
+                                           WriteDataFile.required_params +
+                                           WriteDataFile.optional_params})],
+                         name = "Write_Data_File_FW",
+                         parents = fireworks[-1],
+                         spec = spec)
+
+    fireworks.append(firework2)
+
+    # print("FIREWORKS:")
+    # for firework in fireworks:
+    #     # print(firework.fw_id)
+    #     # print(firework.fw_id % len(fireworks) + 1)
+    #     print(firework.tasks)
+
+    firework_ids = list(links_dict.keys())
+
+    for index, firework_id in enumerate(firework_ids[:-1]):
+        links_dict[firework_id].append(firework_ids[index + 1])
+        # links_dict[firework_id] += firework_ids[index + 1:]
+        # links_dict[firework_id].append(firework2.fw_id)
+    links_dict[firework_ids[-1]].append(firework2.fw_id)
+    # links_dict[firework2.fw_id] = []
+    # print(links_dict)
+
+    return Workflow(fireworks, links_dict)
+
+    # return Workflow(fireworks)
+
+
+if __name__ == "__main__":
+    pass
