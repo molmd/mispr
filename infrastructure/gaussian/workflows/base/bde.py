eb829eab1f0ebe4748067ce8b13284a098e302db
Rasha <atwi.r@husky.neu.edu>
2020-12-19 12:36:15 -0500

    Add an analysis firework for a BDE calc

--- infrastructure/gaussian/workflows/base/bde.py
+++ infrastructure/gaussian/workflows/base/bde.py
@@ -5,6 +5,7 @@ from fireworks import Firework, Workflow
 from infrastructure.gaussian.utils.utils import \
     recursive_relative_to_absolute_path, check_solvent_inputs, \
     add_solvent_inputs, get_job_name
+from infrastructure.gaussian.firetasks.parse_outputs import BDEtoDB
 from infrastructure.gaussian.fireworks.core_standard import BreakMolFW
 from infrastructure.gaussian.workflows.base.core import common_fw, \
     WORKFLOW_KWARGS
@@ -13,6 +14,7 @@ from infrastructure.gaussian.workflows.base.core import common_fw, \
 def get_bde(mol_operation_type,
             mol,
             ref_charge=0,
+            fragment_charges=None,
             bonds=None,
             db=None,
             name="bde_calculation",
@@ -25,13 +27,11 @@ def get_bde(mol_operation_type,
             oxidation_states=None,
             skip_opt_freq=False,
             **kwargs):
-    # break one bond at indexes
-    # optional breaking all bonds
     # optional breaking bonds in subsequent species? NO
     fws = []
     working_dir = working_dir or os.getcwd()
     mol = recursive_relative_to_absolute_path(mol, working_dir)
-
+    gout_key = "ref_mol"
     gins = [opt_gaussian_inputs, freq_gaussian_inputs]
     check_solvent_inputs(gins)
 
@@ -40,8 +40,6 @@ def get_bde(mol_operation_type,
             add_solvent_inputs(gins,
                                solvent_gaussian_inputs,
                                solvent_properties)
-
-    gout_key = "ref_mol"
 
     _, label, opt_freq_fws = common_fw(mol_operation_type=mol_operation_type,
                                        mol=mol,
@@ -63,16 +61,32 @@ def get_bde(mol_operation_type,
                           from_fw_spec=True,
                           bonds=bonds,
                           ref_charge=ref_charge,
+                          fragment_charges=fragment_charges,
                           db=db,
+                          calc_frags=True,
                           opt_gaussian_inputs=opt_gaussian_inputs,
                           freq_gaussian_inputs=freq_gaussian_inputs,
                           cart_coords=cart_coords,
                           name=get_job_name(label, "breaking"),
                           parents=fws[:],
-                          working_dir=os.path.join(working_dir, label,
+                          working_dir=os.path.join(working_dir,
+                                                   label,
                                                    "fragments"),
                           **kwargs)
     fws.append(break_fw)
+
+    fw_analysis = Firework(
+        BDEtoDB(principle_mol_key=gout_key,
+                db=db,
+                solvent_gaussian_inputs=solvent_gaussian_inputs,
+                solvent_properties=solvent_properties,
+                **{i: j for i, j in kwargs.items()
+                   if i in BDEtoDB.required_params +
+                   BDEtoDB.optional_params}),
+        parents=fws[:],
+        name="{}-{}".format(label, "bde_analysis"),
+        spec={'_launch_dir': os.path.join(working_dir, 'analysis')})
+    fws.append(fw_analysis)
 
     return Workflow(fws,
                     name="{}_{}".format(label, name),
