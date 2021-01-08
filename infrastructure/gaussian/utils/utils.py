a62b99c7f886a8b214c0fd247327a49144de8f95
Rasha <atwi.r@husky.neu.edu>
2021-01-08 11:17:25 -0500

    Cleanup utilities module

--- infrastructure/gaussian/utils/utils.py
+++ infrastructure/gaussian/utils/utils.py
@@ -1,6 +1,11 @@
+# coding: utf-8
+
+
+# Contains utility functions useful in many of the infrastructure levels.
+
 import os
-import logging
 import json
+import logging
 import random
 
 import pybel as pb
@@ -19,12 +24,20 @@ from fireworks.utilities.fw_utilities import get_slug
 from fireworks.fw_config import CONFIG_FILE_DIR
 from fireworks import FileWriteTask
 
+__author__ = "Rasha Atwi"
+__maintainer__ = "Rasha Atwi"
+__email__ = "rasha.atwi@stonybrook.edu"
+__status__ = "Development"
+__date__ = "Jan 2021"
+__version__ = 0.2
+
+
 logger = logging.getLogger(__name__)
 
-JOB_TYPES = {'sp', 'opt', 'freq', 'irc', 'ircmax', 'scan', 'polar', 'admp',
-             'bomd', 'eet', 'force', 'stable', 'volume', 'density', 'guess',
-             'pop', 'scrf', 'cphf', 'prop', 'nmr', 'cis', 'zindo', 'td', 'eom',
-             'sac-ci'}
+JOB_TYPES = {"sp", "opt", "freq", "irc", "ircmax", "scan", "polar", "admp",
+             "bomd", "eet", "force", "stable", "volume", "density", "guess",
+             "pop", "scrf", "cphf", "prop", "nmr", "cis", "zindo", "td", "eom",
+             "sac-ci"}
 SCRF_MODELS = {"pcm", "iefpcm", "cpcm", "dipole", "ipcm", "isodensity",
                "scipcm", "smd"}
 
@@ -42,14 +55,14 @@ def process_mol(operation_type, mol, local_opt=False, **kwargs):
         else os.getcwd()
     db = get_db(kwargs["db"]) if "db" in kwargs else get_db()
 
-    if operation_type == 'get_from_mol':
+    if operation_type == "get_from_mol":
         if not isinstance(mol, Molecule):
             raise Exception("mol is not a Molecule object; either "
                             "provide a Molecule object or use another "
                             "operation type with its corresponding inputs")
         output_mol = mol
 
-    elif operation_type == 'get_from_file':
+    elif operation_type == "get_from_file":
         if not os.path.isabs(mol):
             file_path = os.path.join(working_dir, mol)
         else:
@@ -61,7 +74,7 @@ def process_mol(operation_type, mol, local_opt=False, **kwargs):
 
         output_mol = Molecule.from_file(file_path)
 
-    elif operation_type == 'get_from_str':
+    elif operation_type == "get_from_str":
         str_type = kwargs.get("str_type")
         if str_type is None:
             raise ValueError("a mol string format must be specified to process "
@@ -69,7 +82,7 @@ def process_mol(operation_type, mol, local_opt=False, **kwargs):
         output_mol = Molecule.from_str(mol, str_type)
 
     # TODO: change name of this operation_type to reflect db
-    elif operation_type == 'get_from_smiles':
+    elif operation_type == "get_from_smiles":
         # mol = mol_smile
         mol_dict = db.retrieve_molecule(mol)
         if not mol_dict:
@@ -77,7 +90,7 @@ def process_mol(operation_type, mol, local_opt=False, **kwargs):
                 "mol is not found in the database")
         output_mol = Molecule.from_dict(mol_dict)
 
-    elif operation_type == 'get_from_gout':
+    elif operation_type == "get_from_gout":
         if not isinstance(mol, GaussianOutput):
             raise Exception("mol is not a GaussianOutput object; either "
                             "provide a GaussianOutput object or use another "
@@ -85,7 +98,7 @@ def process_mol(operation_type, mol, local_opt=False, **kwargs):
         output_run = mol.as_dict()
         output_mol = Molecule.from_dict(output_run["output"]["molecule"])
 
-    elif operation_type == 'get_from_run_dict':
+    elif operation_type == "get_from_run_dict":
         if not isinstance(mol, dict) and "output" not in mol:
             raise Exception("mol is not a GaussianOutput dictionary; either "
                             "provide a GaussianOutput dictionary or use "
@@ -93,17 +106,17 @@ def process_mol(operation_type, mol, local_opt=False, **kwargs):
                             "inputs")
         output_mol = Molecule.from_dict(mol["output"]["output"]["molecule"])
 
-    elif operation_type == 'get_from_run_id':
+    elif operation_type == "get_from_run_id":
         # mol = run_id
         run = db.runs.find_one({"_id": ObjectId(mol)})
         if not run:
             raise Exception("Gaussian run is not in the database")
-        mol_dict = run['output']['output']['molecule']
+        mol_dict = run["output"]["output"]["molecule"]
         output_mol = Molecule.from_dict(mol_dict)
 
-    elif operation_type == 'get_from_run_query':
-        # mol = {'smiles': smiles, 'type': type, 'functional': func,
-        #        'basis': basis, 'phase': phase, ...}
+    elif operation_type == "get_from_run_query":
+        # mol = {"smiles": smiles, "type": type, "functional": func,
+        #        "basis": basis, "phase": phase, ...}
         logger.info("If the query criteria satisfy more than "
                     "one document, the last updated one will "
                     "be used. To perform a more specific "
@@ -112,15 +125,15 @@ def process_mol(operation_type, mol, local_opt=False, **kwargs):
         run = db.retrieve_run(**mol)
         if not run:
             raise Exception("Gaussian run is not in the database")
-        run = max(run, key=lambda i: i['last_updated'])
-        mol_dict = run['output']['output']['molecule']
+        run = max(run, key=lambda i: i["last_updated"])
+        mol_dict = run["output"]["output"]["molecule"]
         output_mol = Molecule.from_dict(mol_dict)
 
-    elif operation_type == 'derive_molecule':
-        # mol = {'operation_type': 'get_from_file', 'mol': file_path,
-        #        'func_grp': func_group_name, ....}
+    elif operation_type == "derive_molecule":
+        # mol = {"operation_type": "get_from_file", "mol": file_path,
+        #        "func_grp": func_group_name, ....}
 
-        func_grp_name = mol.get('func_grp')
+        func_grp_name = mol.get("func_grp")
         if not func_grp_name:
             raise Exception("No FG provided; Provide the name of the FG "
                             "to be retrieved from the database")
@@ -129,32 +142,32 @@ def process_mol(operation_type, mol, local_opt=False, **kwargs):
             raise Exception("FG is not found in the database")
         fg = Molecule(fg_dict["species"], fg_dict["coords"])
 
-        output_mol = process_mol(operation_type=mol['operation_type'],
-                                 mol=mol['mol'], **kwargs)
+        output_mol = process_mol(operation_type=mol["operation_type"],
+                                 mol=mol["mol"], **kwargs)
         output_mol.substitute(mol["index"], fg, mol["bond_order"])
 
-    elif operation_type == 'link_molecules':
+    elif operation_type == "link_molecules":
         # TODO: add a checking step in the original molecule to make sure no
         #  overlapping happens
-        # mol = {'operation_type': ['get_from_file', 'get_from_smiles'],
-        #        'mol': ['kes/rasa/defe.xyz', 'mol_smiles'],
-        #        'index': [3, 5],
-        #        'bond_order': 2}
+        # mol = {"operation_type": ["get_from_file", "get_from_smiles"],
+        #        "mol": ["kes/rasa/defe.xyz", "mol_smiles"],
+        #        "index": [3, 5],
+        #        "bond_order": 2}
 
-        # mol = {'operation_type': ['get_from_file', 'derive_molecule'],
-        #        'mol': ['kes/rasa/defe.xyz', {'operation_type':
-        #        'get_from_smiles, 'mol': smile}],
-        #        'index': [3, 5],
-        #        'bond_order': 2}
-        linking_mol = process_mol(operation_type=mol['operation_type'][0],
-                                  mol=mol['mol'][0], **kwargs)
-        linked_mol = process_mol(operation_type=mol['operation_type'][1],
-                                 mol=mol['mol'][1], **kwargs)
+        # mol = {"operation_type": ["get_from_file", "derive_molecule"],
+        #        "mol": ["kes/rasa/defe.xyz", {"operation_type":
+        #        "get_from_smiles, "mol": smile}],
+        #        "index": [3, 5],
+        #        "bond_order": 2}
+        linking_mol = process_mol(operation_type=mol["operation_type"][0],
+                                  mol=mol["mol"][0], **kwargs)
+        linked_mol = process_mol(operation_type=mol["operation_type"][1],
+                                 mol=mol["mol"][1], **kwargs)
         output_mol = linking_mol.link(linked_mol, mol["index"][0],
                                       mol["index"][1], mol["bond_order"])
 
     else:
-        raise ValueError(f'operation type {operation_type} is not supported')
+        raise ValueError(f"operation type {operation_type} is not supported")
 
     if local_opt:
         force_field = kwargs.get("force_field", "mmff94")
@@ -171,7 +184,7 @@ def process_run(operation_type, run, input_file=None, **kwargs):
     working_dir = kwargs.get("working_dir", os.getcwd())
     db = get_db(kwargs["db"]) if "db" in kwargs else get_db()
 
-    if operation_type == 'get_from_gout':
+    if operation_type == "get_from_gout":
         if not isinstance(run, GaussianOutput):
             raise Exception("run is not a GaussianOutput object; either "
                             "provide a GaussianOutput object or use another "
@@ -179,7 +192,7 @@ def process_run(operation_type, run, input_file=None, **kwargs):
         gout = run.as_dict()
         gout_dict = _cleanup_gout(gout, working_dir, input_file)
 
-    elif operation_type == 'get_from_file':
+    elif operation_type == "get_from_file":
         if not os.path.isabs(run):
             file_path = os.path.join(working_dir, run)
         else:
@@ -197,23 +210,23 @@ def process_run(operation_type, run, input_file=None, **kwargs):
                              "another operation type with its corresponding "
                              "inputs")
 
-    elif operation_type == 'get_from_run_dict':
+    elif operation_type == "get_from_run_dict":
         if not isinstance(run, dict) and "output" not in run:
             raise Exception("run is not a GaussianOutput dictionary; either"
                             "provide a GaussianOutput dictionary or use another"
                             "operation type with its corresponding inputs")
         gout_dict = run
 
-    elif operation_type == 'get_from_run_id':
+    elif operation_type == "get_from_run_id":
         # run = run_id
         run = db.runs.find_one({"_id": ObjectId(run)})
         if not run:
             raise Exception("Gaussian run is not in the database")
         gout_dict = run
 
-    elif operation_type == 'get_from_run_query':
-        # run = {'smiles': smiles, 'type': type, 'functional': func,
-        #        'basis': basis, 'phase': phase, ...}
+    elif operation_type == "get_from_run_query":
+        # run = {"smiles": smiles, "type": type, "functional": func,
+        #        "basis": basis, "phase": phase, ...}
         logger.info("If the query criteria satisfy more than "
                     "one document, the last updated one will "
                     "be used. To perform a more specific "
@@ -222,11 +235,11 @@ def process_run(operation_type, run, input_file=None, **kwargs):
         run = db.retrieve_run(**run)
         if not run:
             raise Exception("Gaussian run is not in the database")
-        run = max(run, key=lambda i: i['last_updated'])
+        run = max(run, key=lambda i: i["last_updated"])
         gout_dict = run
 
     else:
-        raise ValueError(f'operation type {operation_type} is not supported')
+        raise ValueError(f"operation type {operation_type} is not supported")
     # TODO: find a better way; seems hacky!
     if "_id" in gout_dict:
         gout_dict["_id"] = str(gout_dict["_id"])
@@ -272,7 +285,7 @@ def get_chem_schema(mol):
                      "chemsys": comp.chemical_system,
                      "nsites": mol.num_sites,
                      "nelements":
-                         len(comp.chemical_system.replace('-', ' ').split(' ')),
+                         len(comp.chemical_system.replace("-", " ").split(" ")),
                      "is_ordered": mol.is_ordered,
                      "is_valid": mol.is_valid()})
     return mol_dict
@@ -405,6 +418,15 @@ def draw_rdkit_mol_with_highlighted_bonds(rdkit_mol, bonds,
     return colors
 
 
+def calc_energy(rdkit_mol, maxIters=200):
+    from rdkit.Chem import AllChem
+    AllChem.UFFOptimizeMolecule(rdkit_mol, maxIters)
+    ff = AllChem.UFFGetMoleculeForceField(rdkit_mol)
+    ff.Minimize(maxIts=maxIters)
+    E = ff.CalcEnergy()
+    return E
+
+
 def add_namefile(original_wf, use_slug=True):
     for idx, fw in enumerate(original_wf.fws):
         fname = "FW--{}".format(fw.name)
@@ -479,46 +501,46 @@ def label_atoms(mol):
     mol_smiles = pm.write("smi").strip()
     mol_smiles_copy = mol_smiles.lower()
     counter = 0
-    count_1 = ''
-    count_2 = ''
-    count_3 = ''
+    count_1 = ""
+    count_2 = ""
+    count_3 = ""
     smiles = mol_smiles.lower()
     atoms = [str(i).lower() for i in mol.species]
     sorted_atoms = sorted(atoms, key=lambda x: len(x), reverse=True)
     index_atom_map = {}
-    h_index = [i for i, j in enumerate(mol_smiles_copy) if j == 'h']
+    h_index = [i for i, j in enumerate(mol_smiles_copy) if j == "h"]
     for atom in sorted_atoms:
-        if atom == 'h':
-            smiles = smiles.replace(atom, '!', 1)
+        if atom == "h":
+            smiles = smiles.replace(atom, "!", 1)
             continue
         index = smiles.find(atom)
         if index != -1:
-            smiles = smiles.replace(atom, '!', 1)
+            smiles = smiles.replace(atom, "!", 1)
             index_atom_map[index] = atom
     existing_atoms = [index_atom_map[i] for i in sorted(index_atom_map)]
     for ch in smiles:
-        if ch == '!':
+        if ch == "!":
             flag = 0
             if len(count_1) in h_index:
-                atom = 'h'
+                atom = "h"
                 flag = 1
             else:
                 atom = atoms[counter]
                 while atom not in existing_atoms:
                     counter += 1
                     atom = atoms[counter]
-            if atom.lower() == 'h':
+            if atom.lower() == "h":
                 i = 0
             else:
                 i = counter
             if i < 10:
                 count_1 += f"{i: >{len(atom)}}"
-                count_2 += ' ' * len(atom)
-                count_3 += ' ' * len(atom)
+                count_2 += " " * len(atom)
+                count_3 += " " * len(atom)
             elif i < 100:
                 count_1 += f"{i // 10: >{len(atom)}}"
                 count_2 += f"{i % 10: >{len(atom)}}"
-                count_3 += ' ' * len(atom)
+                count_3 += " " * len(atom)
             else:
                 count_1 += f"{i // 10 // 10: >{len(atom)}}"
                 count_2 += f"{i % 100 // 10: >{len(atom)}}"
@@ -526,9 +548,9 @@ def label_atoms(mol):
             if flag == 0:
                 counter += 1
         else:
-            count_1 += ' '
-            count_2 += ' '
-            count_3 += ' '
+            count_1 += " "
+            count_2 += " "
+            count_3 += " "
     print(f"{mol_smiles}\n{count_1}\n{count_2}\n{count_3}")
 
 
@@ -643,12 +665,12 @@ def add_solvent_to_prop_dict(prop_dict, solvent_gaussian_inputs,
 
 
 def _modify_gout(gout):
-    gout['input']['charge'] = gout['charge']
-    gout['input']['spin_multiplicity'] = gout['spin_multiplicity']
-    del_keys_out = ('nsites', 'unit_cell_formula',
-                    'reduced_cell_formula', 'pretty_formula',
-                    'elements', 'nelements', 'charge',
-                    'spin_multiplicity')
+    gout["input"]["charge"] = gout["charge"]
+    gout["input"]["spin_multiplicity"] = gout["spin_multiplicity"]
+    del_keys_out = ("nsites", "unit_cell_formula",
+                    "reduced_cell_formula", "pretty_formula",
+                    "elements", "nelements", "charge",
+                    "spin_multiplicity")
     [gout.pop(k, None) for k in del_keys_out]
     return gout
 
@@ -660,14 +682,14 @@ def _create_gin(gout, working_dir, input_file):
         else:
             input_path = input_file
         gin = GaussianInput.from_file(input_path).as_dict()
-        gin['nbasisfunctions'] = gout['input']['nbasisfunctions']
-        gin['pcm_parameters'] = gout['input']['pcm_parameters']
+        gin["nbasisfunctions"] = gout["input"]["nbasisfunctions"]
+        gin["pcm_parameters"] = gout["input"]["pcm_parameters"]
         return gin
     else:
-        gin = gout['input']
-        gin['input_parameters'] = None
-        gin['@class'] = 'GaussianInput'
-        gin['@module'] = 'pymatgen.io.gaussian'
+        gin = gout["input"]
+        gin["input_parameters"] = None
+        gin["@class"] = "GaussianInput"
+        gin["@module"] = "pymatgen.io.gaussian"
         logger.info("input parameters at the end of the Gaussian input "
                     "section will not be saved to the database due to "
                     "a missing input file")
@@ -676,33 +698,33 @@ def _create_gin(gout, working_dir, input_file):
 
 def _job_types(gin):
     return list(filter(lambda x: x in {k.lower(): v for k, v in
-                                       gin['route_parameters'].items()},
+                                       gin["route_parameters"].items()},
                        JOB_TYPES))
 
 
 def _cleanup_gout(gout, working_dir, input_file):
     gout = _modify_gout(gout)
     gin = _create_gin(gout, working_dir, input_file)
-    del gout['input']
+    del gout["input"]
     job_types = _job_types(gin)
-    mol = Molecule.from_dict(gout['output']['molecule'])
+    mol = Molecule.from_dict(gout["output"]["molecule"])
     # TODO: check if other solvation models are supported
-    gout_dict = {'input': gin, 'output': gout,
-                 'functional': gin['functional'],
-                 'basis': gin['basis_set'],
-                 'phase': 'solution' if gout['is_pcm'] else 'gas',
-                 'type': ';'.join(job_types),
+    gout_dict = {"input": gin, "output": gout,
+                 "functional": gin["functional"],
+                 "basis": gin["basis_set"],
+                 "phase": "solution" if gout["is_pcm"] else "gas",
+                 "type": ";".join(job_types),
                  **get_chem_schema(mol)}
     gout_dict = {i: j for i, j in gout_dict.items() if i not in
-                 ['sites', '@module', '@class', 'charge',
-                  'spin_multiplicity']}
+                 ["sites", "@module", "@class", "charge",
+                  "spin_multiplicity"]}
     return gout_dict
 
 
 def recursive_signature_remove(d):
     if isinstance(d, dict):
         return {i: recursive_signature_remove(j)
-                for i, j in d.items() if not i.startswith('@')}
+                for i, j in d.items() if not i.startswith("@")}
     else:
         return d
 
