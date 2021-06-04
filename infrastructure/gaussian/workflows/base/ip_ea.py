ad890758e4bab99a2b5a7610f073ddb2557545d5
Rasha <atwi.r@husky.neu.edu>
2021-06-04 12:37:28 -0400

    Handle vertical IP/EA and include standard potentials

--- infrastructure/gaussian/workflows/base/ip_ea.py
+++ infrastructure/gaussian/workflows/base/ip_ea.py
@@ -12,8 +12,9 @@ from copy import deepcopy
 
 from fireworks import Firework, Workflow
 
-from infrastructure.gaussian.utils.utils import \
-    recursive_relative_to_absolute_path, handle_gaussian_inputs
+from infrastructure.gaussian.utilities.files import bibtex_parser, \
+    recursive_relative_to_absolute_path
+from infrastructure.gaussian.utilities.inputs import handle_gaussian_inputs
 from infrastructure.gaussian.firetasks.parse_outputs import IPEAtoDB
 from infrastructure.gaussian.workflows.base.core import common_fw, \
     WORKFLOW_KWARGS
@@ -30,8 +31,8 @@ logger = logging.getLogger(__name__)
 
 class Node:
     def __init__(self, state: str, phase: str, num_electrons: int,
-                 mol_operation_type=None, mol=None, skip_opt_freq=None,
-                 parent: 'Node' = None, ref_charge=None,
+                 mol_operation_type=None, mol=None, skips=None,
+                 check_result=None, parent: 'Node' = None, ref_charge=None,
                  branch_cation_from_anion: bool = False, h_index: list = None):
         self.phase = phase
         self.state = state
@@ -66,7 +67,7 @@ class Node:
             assert mol is not None, 'if parent is None, mol should be given'
             self.mol_operation_type = mol_operation_type
             self.mol = mol
-            self.skip = skip_opt_freq
+            self.check = check_result
             self.process_mol_func = True
             self.from_fw_spec = False
             self.dir_head = None
@@ -91,15 +92,16 @@ class Node:
                 self.mol_operation_type = 'get_from_run_dict'
                 self.mol = self.parent.gout_key
             self.process_mol_func = False
-            self.skip = False
+            self.check = []
             self.from_fw_spec = True
             self.dir_head = self.parent.dir_head
+        self.skip = skips
         self.mol_name = None
 
     def create_fireworks(self, opt_gaussian_inputs, freq_gaussian_inputs,
                          solvent_gaussian_inputs, solvent_properties,
-                         working_dir, db, cart_coords,
-                         branch_cation_from_anion, **kwargs):
+                         working_dir, db, cart_coords, branch_cation_from_anion,
+                         **kwargs):
         if 'mol_name' in kwargs:
             self.mol_name = kwargs['mol_name']
             self.dir_head = kwargs['mol_name']
@@ -119,15 +121,17 @@ class Node:
 
         opt_gins = deepcopy(opt_gaussian_inputs)
         freq_gins = deepcopy(freq_gaussian_inputs)
+        solvent_gins = deepcopy(solvent_gaussian_inputs)
+        solvent_props = deepcopy(solvent_properties)
 
         if self.phase.lower() != 'solution':
-            solvent_gaussian_inputs = None
-            solvent_properties = None
+            solvent_gins = None
+            solvent_props = None
 
         gaussian_inputs = handle_gaussian_inputs({'opt': opt_gins,
                                                   'freq': freq_gins},
-                                                 solvent_gaussian_inputs,
-                                                 solvent_properties)
+                                                 solvent_gins,
+                                                 solvent_props)
         opt_gins = gaussian_inputs['opt']
         freq_gins = gaussian_inputs['freq']
 
@@ -150,7 +154,8 @@ class Node:
                       mol_name=self.mol_name,
                       dir_head=self.dir_head,
                       gout_key=self.gout_key,
-                      skip_opt_freq=self.skip,
+                      skips=self.skip,
+                      check_result=self.check,
                       dir_structure=dir_structure,
                       from_fw_spec=self.from_fw_spec,
                       charge=self.charge,
@@ -162,7 +167,7 @@ class Node:
         return
 
     def branch(self, branching_states, branching_phases, num_of_electrons,
-               branch_cation_from_anion, h_index):
+               branch_cation_from_anion, h_index, vertical):
         if self.state == 'cation':
             branching_states = [i for i in branching_states if i != 'anion']
         if self.state == 'anion':
@@ -170,16 +175,30 @@ class Node:
                 branching_states = [i for i in branching_states if
                                     i != 'cation']
         for state in branching_states:
+            if vertical:
+                if state == 'reference':
+                    skips = None
+                else:
+                    skips = ['opt']
+            else:
+                skips = None
             self.children_nodes.append(Node(
                 state, self.phase, num_of_electrons, parent=self,
                 branch_cation_from_anion=branch_cation_from_anion,
-                h_index=h_index))
+                h_index=h_index, skips=skips))
         if self.phase in branching_phases:
             branching_phases.remove(self.phase)
         for phase in branching_phases:
+            if vertical:
+                if self.state == 'reference':
+                    skips = None
+                else:
+                    skips = ['opt']
+            else:
+                skips = None
             self.children_nodes.append(Node(
                 self.state, phase, 0, parent=self,
-                branch_cation_from_anion=branch_cation_from_anion))
+                branch_cation_from_anion=branch_cation_from_anion, skips=skips))
         return self.children_nodes
 
 
@@ -187,25 +206,26 @@ def get_ip_ea(mol_operation_type,
               mol,
               ref_charge,
               single_step=False,
-              branch_cation_on_anion=False,
+              vertical=False,
+              pcet=False,
               h_index=None,
               num_electrons=1,
+              opt_gaussian_inputs=None,
+              freq_gaussian_inputs=None,
               solvent_gaussian_inputs=None,
               solvent_properties=None,
               states=None,
               phases=None,
               electrode_potentials=None,
               gibbs_elec=-0.001378786,
+              gibbs_h=-0.41816,
               db=None,
               name='ip_ea_calculation',
               working_dir=None,
-              opt_gaussian_inputs=None,
-              freq_gaussian_inputs=None,
               cart_coords=True,
-              skip_opt_freq=False,
+              ref_skips=None,
               **kwargs):
-    # TODO: allow performing vertical IP/EA calculation - all energy
-    #  calculations use neutral state geometry
+    # gibbs_elec and gibbs_h in Ha
     # TODO: HOMO and LUMO of the neutral state as an approximation to IP and EA
     fws = []
     fireworks_dict = {}
@@ -235,17 +255,37 @@ def get_ip_ea(mol_operation_type,
             raise ValueError('The provided phases are not supported. Supported'
                              ' ones are gas and/or solution.')
 
-    if branch_cation_on_anion:
+    if pcet:
         assert h_index is not None, \
             'index at which to attach hydrogen atom should be provided as input'
         assert len(h_index) == num_electrons, \
             'number of indices at which to attach hydrogen atoms should be ' \
             'consistent with number of transfer steps'
 
+    if ref_skips:
+        check_result = ['final_energy', 'Gibbs Free Energy']
+    else:
+        check_result = None
+
+    if 'solution' in phases and not solvent_gaussian_inputs:
+        solvent_gaussian_inputs = '(PCM, Solvent=Water)'
+
+    if electrode_potentials:
+        electrode_potentials = \
+            {k.lower(): {i.lower(): j for i, j in v.items()}
+            if isinstance(v, dict) else v for k, v in
+             electrode_potentials.items()}
+
+        for k, v in electrode_potentials.items():
+            if type(v) != dict or 'potential' and 'ref' not in v:
+                raise KeyError('Standard electrode potential dict should '
+                               'contain potential and ref keys.')
+            electrode_potentials[k]['ref'] = bibtex_parser(v['ref'], working_dir)
+
     root_node = Node('reference', 'gas' if 'gas' in phases else 'solution',
                      0, mol_operation_type, mol_object,
-                     skip_opt_freq=skip_opt_freq, ref_charge=ref_charge,
-                     branch_cation_from_anion=branch_cation_on_anion)
+                     skips=ref_skips, check_result=check_result,
+                     ref_charge=ref_charge, branch_cation_from_anion=pcet)
     solved_nodes = []
     active_nodes = Queue()
     active_nodes.put(root_node)
@@ -253,8 +293,7 @@ def get_ip_ea(mol_operation_type,
         current_node = active_nodes.get()
         current_node.create_fireworks(
             opt_gaussian_inputs, freq_gaussian_inputs, solvent_gaussian_inputs,
-            solvent_properties, working_dir, db, cart_coords,
-            branch_cation_on_anion, **kwargs)
+            solvent_properties, working_dir, db, cart_coords, pcet, **kwargs)
         fws += current_node.fireworks
         fireworks_dict[current_node.gout_key] = current_node.fireworks
         if current_node.parent is not None:
@@ -282,8 +321,7 @@ def get_ip_ea(mol_operation_type,
                 branching_states = deepcopy(states)
 
             if abs(current_node.added_e) == num_electrons:
-                if branch_cation_on_anion and \
-                        current_node.added_h < num_electrons:
+                if pcet and current_node.added_h < num_electrons:
                     branching_states = \
                         [i for i in branching_states if i == 'cation']
                 else:
@@ -292,10 +330,12 @@ def get_ip_ea(mol_operation_type,
                 branching_states = []
 
             branching_phases = [i for i in branching_phases if i in phases]
-            children = current_node.branch(branching_states, branching_phases,
+            children = current_node.branch(branching_states,
+                                           branching_phases,
                                            addition_electrons,
-                                           branch_cation_on_anion,
-                                           h_index)
+                                           pcet,
+                                           h_index,
+                                           vertical)
             for child in children:
                 active_nodes.put(child)
         solved_nodes.append(current_node)
@@ -303,16 +343,18 @@ def get_ip_ea(mol_operation_type,
     gout_keys = [i.gout_key for i in solved_nodes]
     fw_analysis = Firework(
         IPEAtoDB(num_electrons=num_electrons,
-                 steps='single' if single_step else 'multi',
                  states=states,
                  phases=phases,
+                 steps='single' if single_step else 'multi',
+                 root_node_key=root_node.gout_key,
+                 keys=gout_keys,
+                 pcet=pcet,
+                 vertical=vertical,
                  solvent_gaussian_inputs=solvent_gaussian_inputs,
                  solvent_properties=solvent_properties,
                  electrode_potentials=electrode_potentials,
-                 branch_cation_on_anion=branch_cation_on_anion,
-                 keys=gout_keys,
-                 root_node_key=root_node.gout_key,
                  gibbs_elec=gibbs_elec,
+                 gibbs_h=gibbs_h,
                  db=db,
                  **{i: j for i, j in kwargs.items()
                     if i in IPEAtoDB.required_params +
@@ -324,6 +366,7 @@ def get_ip_ea(mol_operation_type,
             '_launch_dir': os.path.join(working_dir, root_node.dir_head,
                                         'analysis')})
     fws.append(fw_analysis)
+
     return Workflow(fws,
                     name='{}_{}'.format(root_node.dir_head, name),
                     links_dict=links_dict,
