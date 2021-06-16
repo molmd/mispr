c1029b2ff28596facf5a7f72d60d18d4af63189e
Rasha <atwi.r@husky.neu.edu>
2021-06-16 13:42:33 -0400

    Get list of fireworks and firetasks matching substrings in a workflow

--- infrastructure/gaussian/utilities/fw_utilities.py
+++ infrastructure/gaussian/utilities/fw_utilities.py
@@ -5,18 +5,39 @@
 
 import logging
 
+from infrastructure.gaussian.firetasks.run_calc import RunGaussianDirect, \
+    RunGaussianCustodian
+
 __author__ = 'Rasha Atwi'
 __maintainer__ = 'Rasha Atwi'
 __email__ = 'rasha.atwi@stonybrook.edu'
 __status__ = 'Development'
 __date__ = 'Jan 2021'
 __version__ = 0.2
+__credits__ = 'Anubhav Jain, Kiran Mathew'
 
 logger = logging.getLogger(__name__)
 
 
-def control_worker(workflow, fworker=None, category=None):
-    list_fireworks_and_tasks = get_list_fireworks_and_tasks(workflow)
+def get_list_fireworks_and_tasks(workflow, firework_substring=None,
+                                 task_substring=None):
+    list_fireworks_and_tasks = []
+    for i_firework, firework in enumerate(workflow.fws):
+        if not firework_substring or \
+                any(n in firework.name for n in firework_substring):
+            for i_task, task in enumerate(firework.tasks):
+                if not task_substring or \
+                        any(n in str(task) for n in task_substring):
+                    list_fireworks_and_tasks.append((i_firework, i_task))
+    return list_fireworks_and_tasks
+
+
+def control_worker(workflow, firework_substring=None, task_substring=None,
+                   fworker=None, category=None):
+    list_fireworks_and_tasks = \
+        get_list_fireworks_and_tasks(workflow,
+                                     firework_substring=firework_substring,
+                                     task_substring=task_substring)
     for i_firework, i_task in list_fireworks_and_tasks:
         if fworker:
             workflow.fws[i_firework].spec['_fworker'] = fworker
@@ -25,16 +46,9 @@ def control_worker(workflow, fworker=None, category=None):
     return workflow
 
 
-def get_list_fireworks_and_tasks(workflow):
-    list_fireworks_and_tasks = []
-    for i_firework, firework in enumerate(workflow.fws):
-        for i_task, task in enumerate(firework.tasks):
-            list_fireworks_and_tasks.append((i_firework, i_task))
-    return list_fireworks_and_tasks
-
-
 def modify_queue_parameters(workflow, ntasks_per_node=None, walltime=None,
-                            queue=None, pre_rocket=None, other_parameters=None):
+                            queue=None, pre_rocket=None, other_parameters=None,
+                            firework_substring=None, task_substring=None):
     queue_parameters = {}
     if ntasks_per_node:
         queue_parameters.update({'ntasks_per_node': ntasks_per_node})
@@ -47,7 +61,10 @@ def modify_queue_parameters(workflow, ntasks_per_node=None, walltime=None,
     if other_parameters:
         queue_parameters.update(other_parameters)
 
-    list_fireworks_and_tasks = get_list_fireworks_and_tasks(workflow)
+    list_fireworks_and_tasks = \
+        get_list_fireworks_and_tasks(workflow,
+                                     firework_substring=firework_substring,
+                                     task_substring=task_substring)
     for i_firework, i_task in list_fireworks_and_tasks:
         workflow.fws[i_firework].spec.update(
             {'_queueadapter': queue_parameters})
