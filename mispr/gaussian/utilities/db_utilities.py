5338e171f93eceddcd0cf787ef7f7377b9aae019
Rasha <atwi.r@husky.neu.edu>
2021-07-27 14:19:46 -0400

    Reformat code

--- mispr/gaussian/utilities/db_utilities.py
+++ mispr/gaussian/utilities/db_utilities.py
@@ -15,7 +15,7 @@ __maintainer__ = "Rasha Atwi"
 __email__ = "rasha.atwi@stonybrook.edu"
 __status__ = "Development"
 __date__ = "Jan 2021"
-__version__ = 0.2
+__version__ = "0.0.1"
 
 logger = logging.getLogger(__name__)
 
@@ -24,8 +24,7 @@ def get_db(input_db=None):
     if not input_db:
         input_db = f"{CONFIG_FILE_DIR}/db.json"
         if not os.path.isfile(input_db):
-            raise FileNotFoundError(
-                "Please provide the database configurations")
+            raise FileNotFoundError("Please provide the database configurations")
     if isinstance(input_db, dict):
         db = GaussianCalcDb(**input_db)
     else:
@@ -36,7 +35,6 @@ def get_db(input_db=None):
 
 def find_calc_in_db(query_criteria, db):
     from mispr.gaussian.utilities.gout import process_run
-    g_out = \
-        process_run(operation_type="get_from_run_query", run=query_criteria,
-                    db=db)
+
+    g_out = process_run(operation_type="get_from_run_query", run=query_criteria, db=db)
     pass
