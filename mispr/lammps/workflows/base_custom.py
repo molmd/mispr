671ca667046b618413c388637f1bf0ca02aa916b
Rasha <atwi.r@husky.neu.edu>
2021-07-27 14:46:03 -0400

    Reformat code

--- mispr/lammps/workflows/base_custom.py
+++ mispr/lammps/workflows/base_custom.py
@@ -2,17 +2,26 @@ import os
 
 from fireworks.core.firework import Firework, Workflow
 
-from mispr.lammps.firetasks.write_inputs import WriteDataFile
 from mispr.lammps.fireworks.custom_fw import GetFFDictFW
+from mispr.lammps.firetasks.write_inputs import WriteDataFile
+
+__author__ = "Matthew Bliss"
+__maintainer__ = "Matthew Bliss"
+__email__ = "matthew.bliss@tufts.edu"
+__status__ = "Development"
+__date__ = "Apr 14, 2020"
+__version__ = "0.0.1"
 
 
-def write_lammps_data(system_species_data,
-                      system_mixture_type,
-                      box_data,
-                      box_data_type = 'cubic',
-                      data_file_name = "complex.data",
-                      working_dir=None,
-                      **kwargs):
+def write_lammps_data(
+    system_species_data,
+    system_mixture_type,
+    box_data,
+    box_data_type="cubic",
+    data_file_name="complex.data",
+    working_dir=None,
+    **kwargs
+):
     """
 
     :param system_species_data: [dict]
@@ -40,8 +49,7 @@ def write_lammps_data(system_species_data,
 
     fireworks = []
     if system_mixture_type == "concentration":
-        system_mixture_data = {"Solutes": {},
-                               "Solvents": {}}
+        system_mixture_data = {"Solutes": {}, "Solvents": {}}
     elif system_mixture_type == "number of molecules":
         system_mixture_data = {}
 
@@ -53,43 +61,58 @@ def write_lammps_data(system_species_data,
         prmtop_filename = "{}.prmtop".format(species)
         inpcrd_filename = "{}.inpcrd".format(species)
 
-        cur_firework = GetFFDictFW(ff_data["molecule"],
-                                   ff_data["ff_param_data"],
-                                   operation_type = ff_data["ff_param_method"],
-                                   label = species,
-                                   working_dir = working_dir,
-                                   output_filename_a = mol2_filename,
-                                   input_filename_p = mol2_filename,
-                                   output_filename_p = frcmod_filename,
-                                   prmtop_filename = prmtop_filename,
-                                   tleap_settings = {"mol2_file_path": os.path.join(working_dir, mol2_filename),
-                                                     "frcmod_file_path": os.path.join(working_dir, frcmod_filename),
-                                                     "prmtop_file_path": os.path.join(working_dir, prmtop_filename),
-                                                     "inpcrd_file_path": os.path.join(working_dir, inpcrd_filename)},
-                                   **kwargs)
+        cur_firework = GetFFDictFW(
+            ff_data["molecule"],
+            ff_data["ff_param_data"],
+            operation_type=ff_data["ff_param_method"],
+            label=species,
+            working_dir=working_dir,
+            output_filename_a=mol2_filename,
+            input_filename_p=mol2_filename,
+            output_filename_p=frcmod_filename,
+            prmtop_filename=prmtop_filename,
+            tleap_settings={
+                "mol2_file_path": os.path.join(working_dir, mol2_filename),
+                "frcmod_file_path": os.path.join(working_dir, frcmod_filename),
+                "prmtop_file_path": os.path.join(working_dir, prmtop_filename),
+                "inpcrd_file_path": os.path.join(working_dir, inpcrd_filename),
+            },
+            **kwargs
+        )
 
         fireworks.append(cur_firework)
 
         links_dict[cur_firework.fw_id] = []
         # print(cur_firework.fw_id)
         if system_mixture_type == "concentration":
-            system_mixture_data[ff_data["mol_mixture_type"]][species] = ff_data["mixture_data"]
+            system_mixture_data[ff_data["mol_mixture_type"]][species] = ff_data[
+                "mixture_data"
+            ]
         elif system_mixture_type == "number of molecules":
             system_mixture_data[species] = ff_data["mixture_data"]
 
     spec = kwargs.pop("spec", {})
-    firework2 = Firework([WriteDataFile(working_dir = working_dir,
-                                        data_filename = data_file_name,
-                                        system_mixture_data_type = system_mixture_type,
-                                        system_mixture_data = system_mixture_data,
-                                        system_box_data = box_data,
-                                        system_box_data_type = box_data_type,
-                                        **{i: j for i, j in kwargs.items() if i in
-                                           WriteDataFile.required_params +
-                                           WriteDataFile.optional_params})],
-                         name = "Write_Data_File_FW",
-                         parents = fireworks[-1],
-                         spec = spec)
+    firework2 = Firework(
+        [
+            WriteDataFile(
+                working_dir=working_dir,
+                data_filename=data_file_name,
+                system_mixture_data_type=system_mixture_type,
+                system_mixture_data=system_mixture_data,
+                system_box_data=box_data,
+                system_box_data_type=box_data_type,
+                **{
+                    i: j
+                    for i, j in kwargs.items()
+                    if i
+                    in WriteDataFile.required_params + WriteDataFile.optional_params
+                }
+            )
+        ],
+        name="Write_Data_File_FW",
+        parents=fireworks[-1],
+        spec=spec,
+    )
 
     fireworks.append(firework2)
 
@@ -110,9 +133,3 @@ def write_lammps_data(system_species_data,
     # print(links_dict)
 
     return Workflow(fireworks, links_dict)
-
-    # return Workflow(fireworks)
-
-
-if __name__ == "__main__":
-    pass
