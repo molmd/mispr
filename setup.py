008a7352bb91e48f5b2b21487204e3194d91d07e
Rasha <atwi.r@husky.neu.edu>
2021-07-27 14:59:43 -0400

    Update setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+
 import setuptools
 
 module_dir = os.path.dirname(os.path.abspath(__file__))
@@ -6,12 +7,11 @@ module_dir = os.path.dirname(os.path.abspath(__file__))
 setuptools.setup(
     name="mispr",
     version="0.0.1",
-    author="Rasha Atwi",
-    author_email="rasha.atwi@tufts.edu",
-    description="mispr contains FireWorks workflows for Materials "
-                "Science",
-    long_description=open(os.path.join(module_dir, 'README.md')).read(),
-    url="https://github.com/tufts-university-rajput-lab/infrastructure",
+    author="Rasha Atwi, Matthew Bliss",
+    author_email="rasha.atwi@stonybrook.edu, matthew.bliss@stonybrook.edu",
+    description="mispr contains FireWorks workflows for Materials Science",
+    long_description=open(os.path.join(module_dir, "README.md")).read(),
+    url="https://github.com/tufts-university-rajput-lab/mispr",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -21,6 +21,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering",
     ],
-    python_requires='>=3.6',
-    package_data={'': ['gaussian/data/*.bib']},
+    python_requires=">=3.6",
+    package_data={"": ["gaussian/data/*.bib"]},
 )
