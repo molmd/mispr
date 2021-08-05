130f34eb416a63363643bc260fd552114a189df9
Rasha <atwi.r@husky.neu.edu>
2021-08-05 12:18:00 -0400

    Start of the hybrid workflow

---
+++ mispr/hybrid/workflows/nmr.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+
+# Defines the DFT-MD workflow that extracts solvation structures and computes their
+# nuclear magnetic resonances.
+
+from mispr.gaussian.workflows.base.nmr import get_nmr_tensors
+
+__author__ = "Rasha Atwi"
+__maintainer__ = "Rasha Atwi"
+__email__ = "rasha.atwi@stonybrook.edu"
+__status__ = "Development"
+__date__ = "Jan 2021"
+__version__ = "0.0.1"
+
+
+def get_solvation_structures_nmr():
+    pass
