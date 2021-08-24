233cb7dec952d1bdbf50367779cb0f79f6536064
rashatwi <atwi.r@husky.neu.edu>
2021-08-24 14:15:51 -0400

    Cleanups

--- mispr/lammps/firetasks/parse_outputs.py
+++ mispr/lammps/firetasks/parse_outputs.py
@@ -23,7 +23,7 @@ from mdproptools.dynamical.diffusion import (
     get_msd_from_dump,
 )
 
-import mispr.lammps.utilities.utils as iluu
+import mispr.lammps.utilities.utilities as iluu
 
 from mispr.lammps.defaults import MSD_SETTINGS, RDF_SETTINGS, DIFF_SETTINGS
 from mispr.gaussian.utilities.metadata import get_mol_formula
