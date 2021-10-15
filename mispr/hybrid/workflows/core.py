58fa4ccde81215872d5c9d6382b3af3709614119
rashatwi <atwi.r@husky.neu.edu>
2021-10-15 12:13:46 -0400

    Change the order and type of fireworks to execute (esp or common_fw)

--- mispr/hybrid/workflows/core.py
+++ mispr/hybrid/workflows/core.py
@@ -1,22 +1,25 @@
 import os
 
-from fireworks import Firework, Workflow
+from copy import deepcopy
 
+from fireworks import Workflow
+
+from mispr.lammps.workflows.base import lammps_workflow
 from mispr.gaussian.utilities.files import recursive_relative_to_absolute_path
 from mispr.gaussian.utilities.inputs import handle_gaussian_inputs
 from mispr.gaussian.workflows.base.esp import get_esp_charges
 from mispr.gaussian.workflows.base.core import common_fw, WORKFLOW_KWARGS
-from mispr.lammps.workflows.base import lammps_workflow
 
 
 def run_hybrid_calcs(
     mol_operation_type,
     mol,
-    ff_method,
-    ff_params,
     mol_type,
     mol_data,
     box_data,
+    ff_method=None,
+    ff_params=None,
+    mixture_type="number of molecules",
     db=None,
     name="hybrid_calculation",
     working_dir=None,
@@ -34,76 +37,87 @@ def run_hybrid_calcs(
     analysis_settings=None,
     **kwargs,
 ):
-    # mol_operation_type = []
-    # mol = []
-    # system_species_data: [dict]
-    #               {
-    #               species1_label: {"molecule": pmg.Molecule,
-    #                                "ff_param_method": str can be one of the
-    #                                     following ("get_from_esp",
-    #                                                "get_from_prmtop",
-    #                                                "get_from_dict")
-    #                                     defaults to "get_from_esp",
-    #                                "ff_param_data": str (path_to_file)
-    #                                                 or dict (unlabeled_ff_dict),
-    #                                "mol_mixture_type": "Solutes" or "Solvents",
-    #                                "mixture_data": int or dict depends on
-    #                                     system_mixture_type},
-    #               ...,
-    #               speciesN_label: {...}
-    #               }
-
     fws = []
     labels = []
     working_dir = working_dir or os.getcwd()
     num_mols = len(mol)
-    gout_keys = [f"mol_{i}" for i in range(1, num_mols + 1)]
+    gout_keys = [f"mol_{i}" for i in range(0, num_mols)]
     if skips is None:
         skips = [None] * num_mols
+    if not ff_method:
+        ff_method = ["get_from_esp"] * num_mols
+        ff_params = [] * num_mols
 
     mol = recursive_relative_to_absolute_path(mol, working_dir)
     gaussian_inputs = handle_gaussian_inputs(
-        {"opt": opt_gaussian_inputs, "freq": freq_gaussian_inputs},
+        {"opt": deepcopy(opt_gaussian_inputs), "freq": deepcopy(freq_gaussian_inputs)},
         solvent_gaussian_inputs,
         solvent_properties,
     )
-    opt_gaussian_inputs = gaussian_inputs["opt"]
-    freq_gaussian_inputs = gaussian_inputs["freq"]
+    opt_gins = gaussian_inputs["opt"]
+    freq_gins = gaussian_inputs["freq"]
 
-    parents = []
+    process_mol_func = kwargs.pop("process_mol_func", True)
+    kwargs.pop("dir_head", None)
+    kwargs.pop("dir_structure", None)
+    charges = kwargs.pop("charge", [0] * num_mols)
 
     # perform geometry optimization and frequency calc for each molecule
-    for [operation, molecule, key, skip, molecule_name, charge] in zip(
-        mol_operation_type,
-        mol,
-        gout_keys,
-        skips,
-        kwargs.pop("mol_name", [None] * num_mols),
-        kwargs.pop("charge", [0] * num_mols),
-    ):
-        _, label, opt_freq_init_fws = common_fw(
-            mol_operation_type=operation,
-            mol=molecule,
-            working_dir=working_dir,
-            db=db,
-            opt_gaussian_inputs=opt_gaussian_inputs,
-            freq_gaussian_inputs=freq_gaussian_inputs,
-            cart_coords=cart_coords,
-            oxidation_states=oxidation_states,
-            gout_key=key,
-            skips=skip,
-            mol_name=molecule_name,
-            charge=charge,
-            **kwargs,
+    for ind, [operation, molecule, key, skip, molecule_name, charge, ff] in enumerate(
+        zip(
+            mol_operation_type,
+            mol,
+            gout_keys,
+            skips,
+            kwargs.pop("mol_name", [None] * num_mols),
+            charges,
+            ff_method,
         )
-        fws += opt_freq_init_fws
-        parents.append(len(fws))
+    ):
+        if ff == "get_from_esp":
+            esp_wf, label = get_esp_charges(
+                mol_operation_type=operation,
+                mol=molecule,
+                db=db,
+                working_dir=working_dir,
+                opt_gaussian_inputs=deepcopy(opt_gaussian_inputs),
+                freq_gaussian_inputs=deepcopy(freq_gaussian_inputs),
+                esp_gaussian_inputs=deepcopy(esp_gaussian_inputs),
+                solvent_gaussian_inputs=solvent_gaussian_inputs,
+                solvent_properties=solvent_properties,
+                cart_coords=cart_coords,
+                oxidation_states=oxidation_states,
+                skips=skip,
+                mol_name=molecule_name,
+                charge=charge,
+                process_mol_func=process_mol_func,
+                gout_keys=[key, f"{key}_esp"],
+                **kwargs,
+            )
+            ff_params[ind] = f"{working_dir}/{label}/ESP/{label}_esp"
+            fws += esp_wf.fws
+        else:
+            _, label, opt_freq_init_fws = common_fw(
+                mol_operation_type=operation,
+                mol=molecule,
+                db=db,
+                working_dir=working_dir,
+                opt_gaussian_inputs=opt_gins,
+                freq_gaussian_inputs=freq_gins,
+                cart_coords=cart_coords,
+                oxidation_states=oxidation_states,
+                gout_key=key,
+                skips=skip,
+                mol_name=molecule_name,
+                charge=charge,
+                process_mol_func=process_mol_func,
+                **kwargs,
+            )
+            fws += opt_freq_init_fws
         labels.append(label)
 
     wf = Workflow(
-        fws,
-        name=name,
-        **{i: j for i, j in kwargs.items() if i in WORKFLOW_KWARGS}
+        fws, name=name, **{i: j for i, j in kwargs.items() if i in WORKFLOW_KWARGS}
     )
 
     # perform esp calc for molecules that user requests amber ff for
