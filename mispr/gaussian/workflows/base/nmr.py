ca0b5fb27d6378dedd3be36af2171b0198f36e8e
rashatwi <atwi.r@husky.neu.edu>
2022-07-26 14:30:07 -0400

    Add nmr wf documentation

--- mispr/gaussian/workflows/base/nmr.py
+++ mispr/gaussian/workflows/base/nmr.py
@@ -42,6 +42,55 @@ def get_nmr_tensors(
     skips=None,
     **kwargs
 ):
+    """
+    Defines a workflow for calculating the nuclear magnetic resonance tensors.
+
+        Firework 1: Optimize the molecule.
+        Firework 2: Calculate the frequencies.
+        Firework 3: Calculate the NMR tensors.
+        Firework 4: Create NMR document/json file.
+
+    Args:
+        mol_operation_type (str): the type of molecule operation. See process_mol defined in mispr/gaussian/utilities/mol.py for supported operations.
+        mol (Molecule, GaussianOutput, str, dict): source of the molecule to be processed. Should match the mol_operation_type.
+        db (str or dict): database credentials; could be provided as the path to the
+            db.json file or in the form of a dictionary; if none is provided, attempts
+            to get it from the configuration files
+        name (str): name of the workflow; defaults to "nmr_tensor_calculation"
+        working_dir (str): path of the working directory where any required input files
+            can be found and output will be created; defaults to the current working directory
+        opt_gaussian_inputs (dict): dictionary of Gaussian input parameters; for example:
+            {
+            "functional": "HF",
+            "basis_set": "6-31G(d)",
+            "route_parameters": {"Opt": None},
+            "link0_parameters": {
+                "%chk": "checkpoint.chk",
+                "%mem": "45GB",
+                "%NProcShared": "28",
+            }
+            default parameters will be used if not specified
+        freq_gaussian_inputs (dict): dictionary of Gaussian input parameters for the
+            frequency step; default parameters will be used if not specified
+        nmr_gaussian_inputs (dict): dictionary of Gaussian input parameters for the
+            NMR step; default parameters will be used if not specified
+        solvent_gaussian_inputs (str): Gaussian input parameters corresponding to the
+            implicit solvent model to be used in the NMR calculations, if any; for example: "(Solvent=TetraHydroFuran)";
+            these parameters should only be specified here and not included in the main
+            gaussian_inputs dictionary for each job (i.e. opt_gaussian_inputs, freq_gaussian_inputs, etc.);
+            defaults to None
+        solvent_properties (dict): additional input parameters to be used in the NMR calculations and
+            relevant to the solvent model, if any; for example, {"EPS":12}; defaults to None
+        cart_coords (bool): uses cartesian coordinates in writing Gaussian input files if set to True,
+            otherwise uses z-matrix; defaults to True
+        oxidation_states (dict): dictionary of oxidation states that can be used in setting the charge and
+            spin multiplicity of the molecule; for example: {"Li":1, "O":-2}; defaults to None
+        skips (list): list of jobs to skip; for example: ["opt", "freq"]; defaults to None
+        **kwargs (keyword arguments): additional kwargs to be passed to the workflow
+
+    Returns:
+        Workflow
+    """
     fws = []
     working_dir = working_dir or os.getcwd()
     mol = recursive_relative_to_absolute_path(mol, working_dir)
