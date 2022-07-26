9f54e8b8b339ef693b77d7f1620694e982cef2ab
rashatwi <atwi.r@husky.neu.edu>
2022-07-26 14:29:54 -0400

    Update esp wf documentation

--- mispr/gaussian/workflows/base/esp.py
+++ mispr/gaussian/workflows/base/esp.py
@@ -47,9 +47,10 @@ def get_esp_charges(
         Firework 1: Optimize the molecule.
         Firework 2: Run a frequency analysis.
         Firework 3: Run an ESP calculation.
+        Firework 4: Create ESP document/json file.
 
     Args:
-        mol_operation_type (str): the type of molecule operation. See mispr/gaussian/utilities/mol/process_mol for supported operations.
+        mol_operation_type (str): the type of molecule operation. See process_mol defined in mispr/gaussian/utilities/mol.py for supported operations.
         mol (Molecule, GaussianOutput, str, dict): source of the molecule to be processed. Should match the mol_operation_type.
         db (str or dict): database credentials; could be provided as the path to the
             db.json file or in the form of a dictionary; if none is provided, attempts
@@ -57,8 +58,7 @@ def get_esp_charges(
         name (str): name of the workflow; defaults to "esp_charges_calculation"
         working_dir (str): path of the working directory where any required input files
             can be found and output will be created; defaults to the current working directory
-        opt_gaussian_inputs (dict): dictionary of Gaussian input parameters for the
-            optimization step of the ESP workflow; for example:
+        opt_gaussian_inputs (dict): dictionary of Gaussian input parameters for the optimization step; for example:
             {
             "functional": "HF",
             "basis_set": "6-31G(d)",
@@ -70,13 +70,13 @@ def get_esp_charges(
             }
             default parameters will be used if not specified
         freq_gaussian_inputs (dict): dictionary of Gaussian input parameters for the
-            frequency step of the ESP workflow; default parameters will be used if not specified
+            frequency step; default parameters will be used if not specified
         esp_gaussian_inputs (dict): dictionary of Gaussian input parameters for the
-            ESP step of the ESP workflow; default parameters will be used if not specified
+            ESP step; default parameters will be used if not specified
         solvent_gaussian_inputs (str): Gaussian input parameters corresponding to the
             implicit solvent model to be used in the ESP calculations, if any; for example: "(Solvent=TetraHydroFuran)";
             these parameters should only be specified here and not included in the main
-            gaussian_inputs dictionary for each job (i.e. opt_gaussian_inputs, freq_esp_gaussian_inputs, etc.);
+            gaussian_inputs dictionary for each job (i.e. opt_gaussian_inputs, freq_gaussian_inputs, etc.);
             defaults to None
         solvent_properties (dict): additional input parameters to be used in the ESP calculations and
             relevant to the solvent model, if any; for example, {"EPS":12}; defaults to None
@@ -84,7 +84,7 @@ def get_esp_charges(
             otherwise uses z-matrix; defaults to True
         oxidation_states (dict): dictionary of oxidation states that can be used in setting the charge and
             spin multiplicity of the molecule; for example: {"Li":1, "O":-2}; defaults to None
-        skips (list): list of jobs to skip; for example: ["freq", "esp"]; defaults to None
+        skips (list): list of jobs to skip; for example: ["opt", "freq"]; defaults to None
         **kwargs (keyword arguments): dditional kwargs to be passed to the workflow
 
     Returns:
