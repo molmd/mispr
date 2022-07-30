3f65c961b609aa4e161be08eb6694c78dcde4204
rashatwi <atwi.r@husky.neu.edu>
2022-07-30 13:36:13 -0400

    Add documentation for the firework that breaks a molecule

--- mispr/gaussian/fireworks/break_mol.py
+++ mispr/gaussian/fireworks/break_mol.py
@@ -26,6 +26,11 @@ FIREWORK_KWARGS = Firework.__init__.__code__.co_varnames
 
 
 class BreakMolFW(Firework):
+    """
+    Processes a molecule input, breaks it into unique fragments, and
+    generates a set of optimization and frequency calculations for
+    each fragment (optional).
+    """
     def __init__(self,
                  mol,
                  mol_operation_type="get_from_mol",
@@ -40,6 +45,42 @@ class BreakMolFW(Firework):
                  working_dir=None,
                  tag="unknown",
                  **kwargs):
+        """
+        Args:
+            mol (Molecule, GaussianOutput, str, dict): source of the
+                molecule to be processed. Should match the mol_operation_type
+            mol_operation_type (str): the type of molecule operation.
+                See process_mol defined in mispr/gaussian/utilities/mol.py
+                for supported operations; defaults to "get_from_mol"
+            bonds (list): list of tuples of the bonds to break; e.g.
+                [(0, 1), (1, 2)] will break the bonds between atoms 0
+                 and 1 and between atoms 1 and 2; if none is specified,
+                 will attempt to break all bonds
+            open_rings (bool): whether to open rings; if set to True, will
+                perform local optimization to get a good initial guess for
+                the structure; defaults to False
+            ref_charge (int): charge on the principle molecule;
+                defaults to 0
+            fragment_charges (list): list of charges to assign to
+                the fragments in addition to the ones already assigned;
+                refer to mispr.gaussian.firetasks.geo_transformation.BreakMolecule
+                for more details
+            calc_frags (bool): whether to create optimization and frequency
+                Fireworks for the generated fragments; defaults to True
+            db (str or dict): database credentials
+            name (str): name of the Firework; defaults to "break_mol"
+            parents (Firework or [Firework]): list of parent FWs this FW
+                depends on
+            working_dir (str): working directory for the calculation;
+                will use the current working directory if not specified
+            tag (str): tag for the calculation; the provided tag will be
+                stored in the db documents for easy retrieval; defaults
+                to "unknown"
+            **kwargs: other kwargs that are passed to:
+                1. Firework.__init__.
+                2. mispr.gaussian.firetasks.geo_transformation.ProcessMoleculeInput
+                3. mispr.gaussian.firetasks.geo_transformation.BreakMolecule
+        """
         t = []
         working_dir = working_dir or os.getcwd()
         if not os.path.exists(working_dir):
