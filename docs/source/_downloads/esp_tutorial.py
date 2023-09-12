b7b024cd8ae3c8b997b7d327e694b6b663a3a918
Rasha Atwi <atwi.r@husky.neu.edu>
2023-09-12 00:16:40 -0400

    Add esp tutorial script for the tutorials page on the website

---
+++ docs/source/_downloads/esp_tutorial.py
@@ -0,0 +1,18 @@
+import os
+
+from fireworks import LaunchPad
+
+from mispr.gaussian.workflows.base.esp import get_esp_charges
+
+
+lpad = LaunchPad.auto_load()
+wf, _ = get_esp_charges(
+    mol_operation_type="get_from_pubchem",
+    mol="monoglyme",
+    format_chk=True,
+    save_to_db=True,
+    save_to_file=True,
+    additional_prop_doc_fields={"name": "monoglyme"},
+    tag="mispr_tutorial",
+)
+lpad.add_wf(wf)
