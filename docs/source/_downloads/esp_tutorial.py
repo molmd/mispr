11d445389d1abe0fab4def2fd50625559dbe35bb
Rasha Atwi <atwi.r@husky.neu.edu>
2023-09-12 09:57:11 -0400

    Update ESP tutorial for the website

--- docs/source/_downloads/esp_tutorial.py
+++ docs/source/_downloads/esp_tutorial.py
@@ -1,13 +1,11 @@
-import os
-
 from fireworks import LaunchPad
 
 from mispr.gaussian.workflows.base.esp import get_esp_charges
 
-
 lpad = LaunchPad.auto_load()
+
 wf, _ = get_esp_charges(
-    mol_operation_type="get_from_pubchem",
+    mol_operation_type="get_from_pubchem",  # (1)!
     mol="monoglyme",
     format_chk=True,
     save_to_db=True,
@@ -15,4 +13,4 @@ wf, _ = get_esp_charges(
     additional_prop_doc_fields={"name": "monoglyme"},
     tag="mispr_tutorial",
 )
-lpad.add_wf(wf)
+lpad.add_wf(wf)  # (2)!
