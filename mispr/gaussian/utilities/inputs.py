85854e48d8033765453a7e526c6c66275f33404d
Rasha Atwi <atwi.r@husky.neu.edu>
2023-09-11 23:21:30 -0400

    Cleanup docstrings

--- mispr/gaussian/utilities/inputs.py
+++ mispr/gaussian/utilities/inputs.py
@@ -1,7 +1,4 @@
-# coding: utf-8
-
-
-# Contains functions for handling gaussian inputs.
+"""Define functions for handling gaussian inputs."""
 
 import logging
 
@@ -23,32 +20,33 @@ def _add_solvent_inputs(
     gaussian_inputs, solvent_gaussian_inputs, solvent_properties=None
 ):
     """
-    Adds solvent inputs to the gaussian_inputs dict. The input
-    parameters relevant to implicit solvation are required to be
-    provided separate from the gaussian_inputs dict to the Gaussian
-    workflows. This is done in order to easily save the solvent
-    properties to the database or to the json files generated from the
-    workflows.
+    Add solvent inputs to the gaussian_inputs dict. The input parameters relevant to
+    implicit solvation are required to be provided separate from the gaussian_inputs
+    dict to the Gaussian workflows. This is done in order to easily save the solvent
+    properties to the database or to the json files generated from the workflows.
 
     Args:
-        gaussian_inputs (dict): dictionary of dictionaries of Gaussian
-            inputs for one or more Gaussian job type, e.g.
-            {"opt": {
-                "functional": "B3LYP",
-                "basis_set": "6-31G",
-                "route_parameters": {
-                    "Opt": "(calcfc, tight)",
-                    "test": None},
-                    }
-            }
-        solvent_gaussian_inputs (str): string of Gaussian inputs for
-            the solvent, e.g. "(Solvent=Generic, Read)"
-        solvent_properties (dict): dictionary of solvent properties,
-            e.g. {"Eps": 4.33, "EpsInf": 1.69}
+        gaussian_inputs (dict): Dictionary of dictionaries of Gaussian inputs for one
+            or more Gaussian job type, e.g.
+
+            .. code-block:: python
+
+                {"opt": {
+                    "functional": "B3LYP",
+                    "basis_set": "6-31G",
+                    "route_parameters": {
+                        "Opt": "(calcfc, tight)",
+                        "test": None},
+                        }
+                }
+
+        solvent_gaussian_inputs (str): String of Gaussian inputs for the solvent, e.g.
+            "(Solvent=Generic, Read)".
+        solvent_properties (dict, optional): Dictionary of solvent properties, e.g.
+            {"Eps": 4.33, "EpsInf": 1.69}.
 
     Returns:
-        dict: dictionary of dictionaries of Gaussian inputs with
-            solvent inputs added
+        dict: Dictionary of dictionaries of Gaussian inputs with solvent inputs added.
     """
     if "generic" in solvent_gaussian_inputs.lower() and not solvent_properties:
         raise Exception(
@@ -68,21 +66,22 @@ def _add_solvent_inputs(
 
 def _check_solvent_inputs(gaussian_inputs):
     """
-    Ensures that implicit solvent parameters are not included in the
-    Gaussian input dictionaries, since those should be provided
-    separately.
+    Ensure that implicit solvent parameters are not included in the Gaussian input
+    dictionaries, since those should be provided separately.
 
     Args:
-        gaussian_inputs (dict): dictionary of dictionaries of Gaussian
-            inputs for one or more Gaussian job type, e.g.
-            {"opt": {
-                "functional": "B3LYP",
-                "basis_set": "6-31G",
-                "route_parameters": {
-                    "Opt": "(calcfc, tight)",
-                    "test": None},
-                    }
-            }
+        gaussian_inputs (dict): Dictionary of dictionaries of Gaussian inputs for one
+            or more Gaussian job type, e.g.
+
+            .. code-block:: python
+                {"opt": {
+                    "functional": "B3LYP",
+                    "basis_set": "6-31G",
+                    "route_parameters": {
+                        "Opt": "(calcfc, tight)",
+                        "test": None},
+                        }
+                }
     """
     route_params = {}
     for key, value in gaussian_inputs.items():
@@ -96,21 +95,25 @@ def _check_solvent_inputs(gaussian_inputs):
 
 def _get_gaussian_inputs(gaussian_inputs, supported_jobs=None):
     """
-    This function is meant to be used in workflows in which multiple
-    Gaussian jobs are performed and the jobs share similar Gaussian
-    keywords; used to handle situations in which the user is
-    not explicitly defining every single job input dictionary.
+    This function is meant to be used in workflows in which multiple Gaussian jobs are
+    performed and the jobs share similar Gaussian keywords; used to handle situations
+    in which the user is not explicitly defining every single job input dictionary.
 
     Args:
-        gaussian_inputs (dict): dictionary of dictionaries of Gaussian
-            input parameters for different job types in a given workflow,
-            e.g. {"opt": {}, "freq": {}, "nmr": {}, "esp": {}, "sp": {}}
-        supported_jobs (dict): dictionary of dictionaries of supported
-            job types and their main inputs,
-            e.g. {"opt": {"Opt": None}, "nmr": {"NMR": "GIAO"}}
+        gaussian_inputs (dict): Dictionary of dictionaries of Gaussian input parameters
+            for different job types in a given workflow, e.g.
+
+            .. code-block:: python
+                {"opt": {}, "freq": {}, "nmr": {}, "esp": {}, "sp": {}}
+
+        supported_jobs (dict, optional): Dictionary of dictionaries of supported job
+            types and their main inputs, e.g.
+
+            .. code-block:: python
+                {"opt": {"Opt": None}, "nmr": {"NMR": "GIAO"}}
 
     Returns:
-        dict: dictionary of dictionaries of Gaussian input parameters
+        dict: Dictionary of dictionaries of Gaussian input parameters.
     """
     supported_jobs = supported_jobs or {}
     supported_jobs = {
@@ -152,22 +155,22 @@ def _get_gaussian_inputs(gaussian_inputs, supported_jobs=None):
 
 def _update_gaussian_inputs(opt_gaussian_inputs, other_gaussian_inputs, main_keyword):
     """
-    Uses the fully defined optimization input parameters to fill in the
-    input parameters for other job types in a given workflow. Done to
-    avoid having to define every single job input dictionary.
+    Use the fully defined optimization input parameters to fill in the input parameters
+    for other job types in a given workflow. Done to avoid having to define every single
+    job input dictionary.
 
     Args:
-        opt_gaussian_inputs (dict): dictionary of Gaussian input
-            parameters for the optimization step of a given workflow
-        other_gaussian_inputs (dict): dictionary of Gaussian inputs
-            other than the route_parameters (e.g. input_parameters) for
-            a job other than optimization in a given workflow
-        main_keyword (dict): simple dictionary containing the main
-            Gaussian route_parameters for the job type,
-            e.g. {"Freq": None} for a frequency analysis
+        opt_gaussian_inputs (dict): Dictionary of Gaussian input parameters for the
+            optimization step of a given workflow.
+        other_gaussian_inputs (dict): Dictionary of Gaussian inputs other than the
+            route_parameters (e.g. input_parameters) for a job other than optimization
+            in a given workflow.
+        main_keyword (dict): Simple dictionary containing the main Gaussian
+            route_parameters for the job type, e.g. {"Freq": None} for a frequency
+            analysis.
 
     Returns:
-
+        dict: Dictionary of Gaussian input parameters for a job other than optimization.
     """
     gaussian_inputs = {**opt_gaussian_inputs, **other_gaussian_inputs}
     if list(main_keyword.keys())[0].lower() not in [
@@ -185,22 +188,32 @@ def handle_gaussian_inputs(
     gaussian_inputs, solvent_gaussian_inputs=None, solvent_properties=None
 ):
     """
-    Wrapper function to cleanup/modify the Gaussian input parameters
-    for one or more job in a workflow. Checks for implicit solvent
-    parameters and adds missing keywords for a given job.
+    Wrapper function to cleanup/modify the Gaussian input parameters for one or more
+    job in a workflow. Checks for implicit solvent parameters and adds missing keywords
+    for a given job.
 
     Args:
-        gaussian_inputs (dict): dictionary of dictionaries of Gaussian
-            inputs, e.g. {"opt": {opt_gaussian_inputs},
-                          "freq": {freq_gaussian_inputs},
-                          etc.}
-        solvent_gaussian_inputs (str): string of Gaussian inputs for
-            the solvent, e.g. "(Solvent=Generic, Read)"
-        solvent_properties (dict): dictionary of solvent properties,
-            e.g. {"Eps": 4.33, "EpsInf": 1.69}
+        gaussian_inputs (dict): Dictionary of dictionaries of Gaussian inputs, e.g.
+
+            .. code-block:: python
+
+                {"opt": {opt_gaussian_inputs}, "freq": {freq_gaussian_inputs}}
+
+        solvent_gaussian_inputs (str, optional): String of Gaussian inputs for the
+            solvent, e.g.
+
+            .. code-block:: python
+
+                "(Solvent=Generic, Read)"
+
+        solvent_properties (dict, optional): Dictionary of solvent properties, e.g.
+
+            .. code-block:: python
+
+                {"Eps": 4.33, "EpsInf": 1.69}
 
     Returns:
-        dict: dictionary of dictionaries of reformatted Gaussian inputs
+        dict: Dictionary of dictionaries of reformatted Gaussian inputs.
     """
     gaussian_inputs = _get_gaussian_inputs(gaussian_inputs)
     _check_solvent_inputs(gaussian_inputs)
